a8bd44f36735329264f09a235e6044b62183baf1
Daniel Frett <daniel.frett@ccci.org>
2011-03-28 15:51:34 -0400

    updated PasswordEqualsTest to actually test encoding of passwords

--- thekey-cas-server-webapp/src/test/java/org/ccci/gcx/idm/web/test/PasswordEqualsTest.java
+++ thekey-cas-server-webapp/src/test/java/org/ccci/gcx/idm/web/test/PasswordEqualsTest.java
@@ -1,30 +1,38 @@
 package org.ccci.gcx.idm.web.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import junit.framework.Assert;
+import junit.framework.TestCase;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.ccci.gcx.idm.common.spring2.test.AbstractTransactionalTestCase;
 import org.ccci.gcx.idm.core.Constants;
 import org.springframework.ldap.filter.EqualsFilter;
-import org.springframework.ldap.filter.Filter;
 
-public class PasswordEqualsTest extends AbstractTransactionalTestCase
-{
+public class PasswordEqualsTest extends TestCase {
     protected static final Log log = LogFactory.getLog( PasswordEqualsTest.class ) ;
 
-    
-    public void testEquals()
-    {
-        /*= INFO =*/ if ( log.isInfoEnabled() ) log.info( "***** BEGIN: testEquals" ) ;
-        
-        Filter passwordFilter = new EqualsFilter( Constants.LDAP_KEY_PASSWORD, "Kendog" ) ;
-        System.out.println("Encoded 'Kendog' = "+passwordFilter.encode());
-        
-        passwordFilter = new EqualsFilter( Constants.LDAP_KEY_PASSWORD, "Kendog!" ) ;
-        System.out.println("Encoded 'Kendog!' = "+passwordFilter.encode());
+    public void testEquals() {
+	log.info("***** BEGIN: testEquals");
 
-        
-        /*= INFO =*/ if ( log.isInfoEnabled() ) log.info( "***** END: testEquals" ) ;
+	// generate a list of passwords to test
+	Map<String, String> passwords = new HashMap<String, String>();
+	passwords.put("Kendog", "Kendog");
+	passwords.put("Kendog!", "Kendog!");
+	passwords.put("Kendog)", "Kendog\\29");
+
+	// test all defined passwords
+	for (Map.Entry<String, String> password : passwords.entrySet()) {
+	    EqualsFilter filter = new EqualsFilter(Constants.LDAP_KEY_PASSWORD,
+		    password.getKey());
+	    System.out.println("Encoded '" + password.getKey() + "' as "
+		    + filter.encode());
+	    Assert.assertEquals("(" + Constants.LDAP_KEY_PASSWORD + "="
+		    + password.getValue() + ")", filter.encode());
+	}
+
+	log.info("***** END: testEquals");
     }
 }
