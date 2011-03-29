39ba54a0d61740bfa844d5528a5758ba0831af92
Daniel Frett <daniel.frett@ccci.org>
2011-03-29 14:17:05 -0400

    remove unused Cookie transform methods

--- thekey-cas-server-webapp/src/main/java/org/ccci/gcx/idm/web/IdmUtil.java
+++ thekey-cas-server-webapp/src/main/java/org/ccci/gcx/idm/web/IdmUtil.java
@@ -2,8 +2,6 @@ package org.ccci.gcx.idm.web;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,7 +15,6 @@ import org.ccci.gcx.idm.core.authentication.client.impl.CasAuthenticationRequest
 import org.ccci.gcx.idm.core.model.impl.GcxUser;
 import org.ccci.gcx.idm.core.service.GcxUserService;
 import org.ccci.gcx.idm.core.util.AuthenticationRequestBuilder;
-import org.springframework.web.util.HtmlUtils;
 
 /**
  * provides various utility methods used by idmweb.
@@ -86,75 +83,6 @@ public final class IdmUtil implements AuthenticationRequestBuilder {
 		return casreq;
 		
 	}
-	
-	//from googlecode
-    public static org.apache.commons.httpclient.Cookie transformServletCookie(javax.servlet.http.Cookie servletCookie) {
-
-        org.apache.commons.httpclient.Cookie newCookie = null;
-
-        if (servletCookie != null) {
-            newCookie = 
-                    new org.apache.commons.httpclient.Cookie(HtmlUtils.htmlEscape(servletCookie.getDomain()), 
-                    		HtmlUtils.htmlEscape(servletCookie.getName()), 
-                    				HtmlUtils.htmlEscape(servletCookie.getValue()), 
-                    						HtmlUtils.htmlEscape(servletCookie.getPath()) != 
-                                                             null ? 
-                                            HtmlUtils.htmlEscape(servletCookie.getPath()) : 
-                                                             "/", 
-                                                             servletCookie.getMaxAge(), 
-                                                             false);
-            //TODO :::  last field should be: servletCookie.getSecure()
-        }
-        return newCookie;
-    }
-
-    /**
-     * Transforms Apache cookies into Servlet Cookies
-     * 
-     * @param apacheCookie apache cookie 
-     * 
-     * @return servlet cookie
-     */
-    public static javax.servlet.http.Cookie transformApacheCookie(org.apache.commons.httpclient.Cookie apacheCookie) {
-
-        javax.servlet.http.Cookie newCookie = null;
-
-        if (apacheCookie != null) {
-            Date expire = apacheCookie.getExpiryDate();
-            int maxAge = -1;
-
-            if (expire == null) {
-                maxAge = -1;
-            } else {
-                Date now = Calendar.getInstance().getTime();
-                // Convert milli-second to second
-                Long second = 
-                    new Long((expire.getTime() - now.getTime()) / 1000);
-                maxAge = second.intValue();
-            }
-
-            newCookie = 
-                    new javax.servlet.http.Cookie(HtmlUtils.htmlEscape(apacheCookie.getName()), 
-                    		HtmlUtils.htmlEscape(apacheCookie.getValue()));
-
-            newCookie.setDomain(HtmlUtils.htmlEscape(apacheCookie.getDomain())!=null ? HtmlUtils.htmlEscape(apacheCookie.getDomain()):"");
-            newCookie.setPath(HtmlUtils.htmlEscape(apacheCookie.getPath()));
-            newCookie.setMaxAge(maxAge);
-            newCookie.setSecure(false);    //TODO:::   (apacheCookie.getSecure());
-            if(log.isDebugEnabled()) log.debug("NEW COOKIE ---");
-            if(log.isDebugEnabled()) log.debug("Name:"+newCookie.getName());
-            if(log.isDebugEnabled()) log.debug("Val:"+newCookie.getValue());
-            if(log.isDebugEnabled()) log.debug("Domain:"+newCookie.getDomain());
-            if(log.isDebugEnabled()) log.debug("Path:"+newCookie.getPath());
-            if(log.isDebugEnabled()) log.debug("MaxAge:"+newCookie.getMaxAge());
-    
-
-        
-        }
-        return newCookie;
-    }
-
-
 
     /**
      * Returns the email address in the message field. or returns null.
