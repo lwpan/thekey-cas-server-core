de5f8b5707fa3e0e458a75b77da483a26ac3a495
Daniel Frett <daniel.frett@ccci.org>
2011-03-29 14:58:38 -0400

    added methods that will set and retrieve an HttpClient 4 object

--- thekey-cas-server-webapp/src/main/java/org/ccci/gcx/idm/web/css/AbstractCssScrubber.java
+++ thekey-cas-server-webapp/src/main/java/org/ccci/gcx/idm/web/css/AbstractCssScrubber.java
@@ -6,6 +6,17 @@ import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.params.SyncBasicHttpParams;
+import org.apache.http.protocol.HTTP;
 import org.ccci.gcx.idm.web.Constants;
 
 /**
@@ -15,6 +26,7 @@ import org.ccci.gcx.idm.web.Constants;
  */
 public abstract class AbstractCssScrubber
 {
+    private HttpClient httpClient;
 
 	private HostConfiguration hc = new HostConfiguration();
 	private HttpConnectionManager hcm = new MultiThreadedHttpConnectionManager();
@@ -55,7 +67,50 @@ public abstract class AbstractCssScrubber
 	{
 		proxyPort = a_port;
 	}
-	
+
+    /**
+     * @return the httpClient
+     */
+    protected HttpClient getHttpClient() {
+	// Create HTTP client if client doesn't exist yet
+	if (this.httpClient == null) {
+	    final SyncBasicHttpParams params = new SyncBasicHttpParams();
+	    HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+	    HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+	    HttpProtocolParams.setUseExpectContinue(params, true);
+	    final SchemeRegistry registry = new SchemeRegistry();
+	    registry.register(new Scheme("http", 80, PlainSocketFactory
+		    .getSocketFactory()));
+	    registry.register(new Scheme("https", 443, SSLSocketFactory
+		    .getSocketFactory()));
+	    final ThreadSafeClientConnManager cm = new ThreadSafeClientConnManager(
+		    registry);
+	    cm.setDefaultMaxPerRoute(10);
+	    cm.setMaxTotal(100);
+	    final DefaultHttpClient client = new DefaultHttpClient(cm, params);
+
+	    // synchronize this code to prevent race condition of paving over an
+	    // HttpClient set by another thread while this method was executing
+	    synchronized (this) {
+		// Make sure we still don't have an HttpClient before setting
+		// one
+		if (this.httpClient == null) {
+		    this.setHttpClient(client);
+		}
+	    }
+	}
+
+	return this.httpClient;
+    }
+
+    /**
+     * @param httpClient
+     *            the HttpClient object to use
+     */
+    public synchronized void setHttpClient(final HttpClient httpClient) {
+	this.httpClient = httpClient;
+    }
+
 	/**
 	 * prepares and returns an httpclient ready for use.
 	 * @return
