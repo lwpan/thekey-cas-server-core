6e295efa9300b6f47c084be4486739c40e80dc53
Daniel Frett <daniel.frett@ccci.org>
2011-03-30 13:01:32 -0400

    updated proxy logic to support HttpClient4

--- thekey-cas-server-webapp/src/main/java/org/ccci/gcx/idm/web/css/AbstractCssScrubber.java
+++ thekey-cas-server-webapp/src/main/java/org/ccci/gcx/idm/web/css/AbstractCssScrubber.java
@@ -6,6 +6,7 @@ import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpHost;
 import org.apache.http.HttpVersion;
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.scheme.PlainSocketFactory;
@@ -20,53 +21,47 @@ import org.apache.http.protocol.HTTP;
 import org.ccci.gcx.idm.web.Constants;
 
 /**
- * provides a proxied httpclient pool usable for fetching css by subclassed cssScrubbers.
- * @author ken
- *
+ * provides a proxied httpclient pool usable for fetching css by subclassed
+ * cssScrubbers.
+ * 
+ * @author Ken Burcham, Daniel Frett
  */
-public abstract class AbstractCssScrubber
-{
+public abstract class AbstractCssScrubber {
+    protected static final Log log = LogFactory
+	    .getLog(AbstractCssScrubber.class);
     private HttpClient httpClient;
 
 	private HostConfiguration hc = new HostConfiguration();
 	private HttpConnectionManager hcm = new MultiThreadedHttpConnectionManager();
-	private String proxyUrl  = null; 
-	private int    proxyPort = Constants.DEFAULTPROXY; //default
-	
-	protected static final Log log = LogFactory.getLog(AbstractCssScrubber.class);
+    private String proxyUrl = null;
+    private int proxyPort = Constants.DEFAULTPROXY;
 
-	
-	private void configureProxy()
-	{
-		if(StringUtils.isNotBlank(proxyUrl))
-		{
-			if(log.isDebugEnabled()) log.debug("PROXY is configured. Using: "+proxyUrl);
-			hc.setProxy(proxyUrl, proxyPort);
-		}
-		else
-			if(log.isDebugEnabled()) log.debug("NO PROXY configured. ");
+    /**
+     * url should be like: proxy.ccci.org (not http://proxy.ccci.org). If a
+     * proxy isn't set then the HttpClient will use a direct connection.
+     * 
+     * @param url
+     */
+    public void setProxyUrl(final String url) {
+	log.debug("SETTING PROXY: " + url);
+	this.proxyUrl = url;
+    }
+
+    /**
+     * port should be 80 or 8080, etc.
+     * 
+     * @param port
+     */
+    public void setProxyPort(final int port) {
+	this.proxyPort = port;
+    }
+
+    protected HttpHost getProxy() {
+	if (StringUtils.isNotBlank(this.proxyUrl)) {
+	    return new HttpHost(this.proxyUrl, this.proxyPort);
 	}
-	
-	/**
-	 * a_url should be like: proxy.ccci.org (not http://proxy.ccci.org).
-	 * If a proxy isn't set then the HttpClient will use a direct connection.
-	 * @param a_url
-	 */
-	public void setProxyUrl(String a_url)
-	{
-		log.debug("SETTING PROXY: "+a_url);
-		proxyUrl = a_url;
-		configureProxy();
-	}
-	
-	/**
-	 * port should be 80 or 8080, etc.
-	 * @param a_port
-	 */
-	public void setProxyPort(int a_port)
-	{
-		proxyPort = a_port;
-	}
+	return null;
+    }
 
     /**
      * @return the httpClient
