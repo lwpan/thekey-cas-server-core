bd5e0601e9d921b3a519defbe361d4f8b24d6c97
Daniel Frett <daniel.frett@ccci.org>
2011-04-14 17:25:25 -0400

    updated the LanguageListBean to use a TreeMap which is sorted

--- thekey-cas-server-webapp/src/main/java/org/ccci/gcx/idm/web/LanguageListBean.java
+++ thekey-cas-server-webapp/src/main/java/org/ccci/gcx/idm/web/LanguageListBean.java
@@ -1,8 +1,8 @@
 package org.ccci.gcx.idm.web;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.TreeMap;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -15,53 +15,63 @@ import org.springframework.core.io.support.PropertiesLoaderUtils;
  * @author Ken Burcham, Daniel Frett
  */
 public class LanguageListBean {
+    protected static final Log log = LogFactory.getLog(LanguageListBean.class);
 
-	//di
-	private String location = Constants.DEFAULT_MESSAGES_LOCATION;
-	private HashMap<String,String> languages = null;
-	
-	protected static final Log log = LogFactory.getLog(LanguageListBean.class);
-	
+    private String location;
+    final private TreeMap<String, String> languages = new TreeMap<String, String>();
+
+    public LanguageListBean() {
+	this.setLocation(Constants.DEFAULT_MESSAGES_LOCATION);
+    }
+
+    /**
+     * load the languages from the current location
+     */
+    protected synchronized void loadLanguages() {
+	log.debug("Loading language list");
+	this.languages.clear();
+	try {
+	    Properties p = PropertiesLoaderUtils
+		    .loadAllProperties(this.location);
+
+	    // store all properties in a TreeMap
+	    for (Object key : p.keySet()) {
+		if (log.isDebugEnabled())
+		    log.debug("Adding language: "
+			    + p.getProperty(key.toString()));
+		languages.put(key.toString().trim().toLowerCase(),
+			p.getProperty(key.toString().trim()));
+	    }
+	} catch (Exception e) {
+	    log.error("Exception trying to load languages.", e);
+	}
+    }
+
+    /**
+     * force a reload of the languages
+     */
+    public void reloadConfiguration() {
+	this.loadLanguages();
+    }
+
+    @Deprecated
     public Map<String, String> getLanguageList() {
-		if(languages == null)
-		{
-			reloadConfiguration();
-		}
-		
-		if(log.isDebugEnabled()) log.debug("Returning languages: "+languages.size());
-		
-		return languages;	
+	return this.getLanguages();
+    }
+
+    /**
+     * @return the languages
+     */
+    public Map<String, String> getLanguages() {
+	if (log.isDebugEnabled()) {
+	    log.debug("Returning languages: " + this.languages.size());
 	}
-	
-	/**
-	 * load the languages fresh.
-	 */
-	public void reloadConfiguration()
-	{
-		if(log.isDebugEnabled()) log.debug("Reloading language list");
-		languages = new HashMap<String,String>();
-		try
-		{
-			Properties p = PropertiesLoaderUtils.loadAllProperties(location);
-		
-			for(Object key : p.keySet())
-			{
-				languages.put(key.toString().trim().toLowerCase(), 
-						      p.getProperty(key.toString().trim()));
-				if(log.isDebugEnabled()) log.debug("Adding language: "+p.getProperty(key.toString()));
-			}
-		}
-		catch(Exception e)
-		{
-			log.error("Exception trying to load languages.",e);
-		}
-	
-	}
-	
-	public void setLocation(String a_loc)
-	{
-		location = a_loc;
-		this.reloadConfiguration();
-	}
-	
+
+	return this.languages;
+    }
+
+    public synchronized void setLocation(String location) {
+	this.location = location;
+	this.loadLanguages();
+    }
 }
