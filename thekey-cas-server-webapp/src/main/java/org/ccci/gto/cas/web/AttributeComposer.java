eae7473266d4f1ba82ea443aecc1abd85f29d38a
Daniel Frett <daniel.frett@ccci.org>
2011-04-27 14:48:44 -0400

    update the AttributeComposer to utilize the generic ServerConfigList object for interacting with the whitelist

--- thekey-cas-server-webapp/src/main/java/org/ccci/gto/cas/web/AttributeComposer.java
+++ thekey-cas-server-webapp/src/main/java/org/ccci/gto/cas/web/AttributeComposer.java
@@ -1,15 +1,14 @@
 package org.ccci.gto.cas.web;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.validation.constraints.NotNull;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.ccci.gcx.idm.core.model.impl.GcxUser;
-import org.ccci.gcx.idm.web.config.XmlConfigurator;
+import org.ccci.gto.cas.config.ServerConfigList;
 
 /**
  * composes attributes that we'll insert into a validation result. A list of
@@ -20,10 +19,7 @@ import org.ccci.gcx.idm.web.config.XmlConfigurator;
  * @author Daniel Frett
  */
 public class AttributeComposer {
-    private static final Log log = LogFactory.getLog(AttributeComposer.class);
-
-    private static final String CONFIG_SERVERELEMENT = "server";
-    private static final String DEFAULT_WHITELIST = "classpath:config/whitelist.xml";
+    protected final Log log = LogFactory.getLog(getClass());
 
     /** Constant representing the guid in the attributes. */
     private static final String ATTRIBUTES_GUID = "ssoGuid";
@@ -37,17 +33,10 @@ public class AttributeComposer {
     /** Constant representing the last name in the attributes. */
     private static final String ATTRIBUTES_LASTNAME = "lastName";
 
+
     /** whitelist of servers authorized for additional attributes. */
-    private final ArrayList<String> whitelist = new ArrayList<String>();
-    private String whitelistLocation;
-
-    public AttributeComposer() {
-	this(DEFAULT_WHITELIST);
-    }
-
-    public AttributeComposer(String location) {
-	this.setWhitelist(location);
-    }
+    @NotNull
+    private ServerConfigList whitelist;
 
     /**
      * returns a Map representing the <cas:attribute> entities for the specified
@@ -68,7 +57,7 @@ public class AttributeComposer {
 	attrs.put(ATTRIBUTES_ADDITIONALGUIDS, user.getGUIDAdditionalString());
 
 	// only authorized services get extended data
-	if (this.inWhitelist(service)) {
+	if (this.whitelist.inList(service)) {
 	    attrs.put(ATTRIBUTES_FIRSTNAME, user.getFirstName());
 	    attrs.put(ATTRIBUTES_LASTNAME, user.getLastName());
 	}
@@ -78,68 +67,10 @@ public class AttributeComposer {
     }
 
     /**
-     * method that loads the whitelist into memory
+     * @param whitelist
+     *            the whitelist to use
      */
-    private synchronized void loadWhitelist() {
-	if (log.isDebugEnabled()) {
-	    log.debug("loading whitelist from: " + this.whitelistLocation);
-	}
-
-	try {
-	    // clear any existing parameters in the whitelist
-	    this.whitelist.clear();
-
-	    // load the config
-	    XmlConfigurator config = new XmlConfigurator(this.whitelistLocation);
-	    config.refresh();
-
-	    // expand the list to include some possible variations.
-	    // TODO: this code needs to become much more robust
-	    for (String authority : config
-		    .getListAsString(CONFIG_SERVERELEMENT)) {
-		if (authority.startsWith("www.")) {
-		    this.whitelist.add(authority);
-		    this.whitelist.add(authority.substring(4));
-		} else {
-		    this.whitelist.add(authority);
-		    this.whitelist.add("www." + authority);
-		}
-	    }
-	} catch (Exception e) {
-	    this.whitelist.clear();
-	    log.error("There was a problem loading the whitelist.", e);
-	}
-    }
-
-    /**
-     * determines if a particular service is in the extended service list
-     * 
-     * @param service
-     * @return
-     */
-    private boolean inWhitelist(String service) {
-	try {
-	    // parse the service to extract the authority
-	    final URL url = new URL(service);
-
-	    // return true if the authority is in the whitelist
-	    if (this.whitelist.contains(url.getAuthority())) {
-		return true;
-	    }
-	} catch (MalformedURLException e) {
-	    if (log.isDebugEnabled())
-		log.debug("Could not parse service to URL: " + service);
-	}
-
-	return false;
-    }
-
-    /**
-     * @param whitelistLocation
-     *            the whitelistLocation to set
-     */
-    public void setWhitelist(String location) {
-	this.whitelistLocation = location;
-	this.loadWhitelist();
+    public void setWhitelist(ServerConfigList whitelist) {
+	this.whitelist = whitelist;
     }
 }
