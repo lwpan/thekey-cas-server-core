d5d4e5f0e2012d49cf1fbe573b41f5e7127946aa
Daniel Frett <daniel.frett@ccci.org>
2011-05-05 14:28:33 -0400

    make the send method synchronized to make the MailSender more thread-safe

--- thekey-cas-server-common/src/main/java/org/ccci/gcx/idm/common/mail/impl/MailSenderImpl.java
+++ thekey-cas-server-common/src/main/java/org/ccci/gcx/idm/common/mail/impl/MailSenderImpl.java
@@ -4,13 +4,15 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.ccci.gcx.idm.common.mail.JavaMailSender;
 import org.ccci.gcx.idm.common.mail.MailSenderTemplate;
+import org.ccci.gcx.idm.common.mail.TemplateMessagePreparator;
 import org.ccci.gcx.idm.common.model.impl.OutgoingMailMessage;
 
 /**
  * <b>MailSenderImpl</b> is a concrete implementation of {@link MailSender} that
  * uses {@link JavaMailSender}.
- *
- * @author Greg Crider  December 1, 2008  5:29:26 PM
+ * 
+ * @author Greg Crider December 1, 2008 5:29:26 PM
+ * @author Daniel Frett
  */
 public class MailSenderImpl extends AbstractMailSender
 {
@@ -35,26 +37,29 @@ public class MailSenderImpl extends AbstractMailSender
         this.m_JavaMailSender = a_javaMailSender ;
     }
 
-
     /**
      * Send an e-mail, using the specified template.
      * 
-     * @param a_Template Template to be used in the construction of the e-mail message.
-     * @param a_OutgoingMailMessage Contains, among other things, the data to be plugged into the
-     *        e-mail template.
+     * @param template
+     *            Template to be used in the construction of the e-mail message.
+     * @param message
+     *            Contains, among other things, the data to be plugged into the
+     *            e-mail template.
      */
     @Override
-    public void send( MailSenderTemplate a_MailSenderTemplate, OutgoingMailMessage a_OutgoingMailMessage )
-    {
-        // Put the necessary information into the preparator
-        this.getMessagePreparator().setMailSenderTemplate( a_MailSenderTemplate ) ;
-        this.getMessagePreparator().setOutgoingMailMessage( a_OutgoingMailMessage ) ;
-        
-        // Send the e-mail
-        /*= INFO =*/ log.info( "Sending e-mail to \"" + a_OutgoingMailMessage.getTo() + "\"" ) ;
-        
-         this.m_JavaMailSender.send( this.getMessagePreparator() ) ;
- 
-    }
+    public synchronized void send(final MailSenderTemplate template,
+	    final OutgoingMailMessage message) {
+	final TemplateMessagePreparator preparator = this
+		.getMessagePreparator();
 
+	// Put the necessary information into the preparator
+	preparator.setMailSenderTemplate(template);
+	preparator.setOutgoingMailMessage(message);
+
+	// Send the e-mail
+	if (log.isInfoEnabled()) {
+	    log.info("Sending e-mail to \"" + message.getTo() + "\"");
+	}
+	this.m_JavaMailSender.send(preparator);
+    }
 }
