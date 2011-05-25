413d8c49aa0ed04c164c40a974a5adf01a57e2a2
Daniel Frett <daniel.frett@ccci.org>
2011-05-25 14:51:18 -0400

    updated RandomPasswordGeneratorTest to utilize the logger provided by the abstract parent class

--- thekey-cas-server-core/src/test/java/org/ccci/gcx/idm/core/util/RandomPasswordGeneratorTest.java
+++ thekey-cas-server-core/src/test/java/org/ccci/gcx/idm/core/util/RandomPasswordGeneratorTest.java
@@ -1,7 +1,5 @@
 package org.ccci.gcx.idm.core.util;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.ccci.gcx.idm.common.spring2.test.AbstractTransactionalTestCase;
 import org.ccci.gcx.idm.core.Constants;
 
@@ -12,20 +10,17 @@ import org.ccci.gcx.idm.core.Constants;
  */
 public class RandomPasswordGeneratorTest extends AbstractTransactionalTestCase
 {
-    protected static final Log log = LogFactory.getLog( RandomGUIDTest.class ) ;
-
-    
     public void testBasic()
     {
-        /*= INFO =*/ if ( log.isInfoEnabled() ) log.info( "***** BEGIN: testBasic" ) ;
+	logger.info("***** BEGIN: testBasic");
         
         RandomPasswordGenerator rpg = (RandomPasswordGenerator)this.getApplicationContext().getBean( Constants.BEAN_RANDOM_PASSWORD ) ;
         
         for( int i=0; i<10; i++ ) {
             String password = rpg.generatePassword( 10 ) ;
-            /*= INFO =*/ if ( log.isInfoEnabled() ) log.info( "***** Password: " + password ) ;
+	    logger.info("***** Password: " + password);
         }
         
-        /*= INFO =*/ if ( log.isInfoEnabled() ) log.info( "***** END: testBasic" ) ;
+	logger.info("***** END: testBasic");
     }
 }
