9fbba11211081bda57fe8389c489b4cb8d498952
Daniel Frett <daniel.frett@ccci.org>
2011-05-25 14:49:27 -0400

    updated the AbstractTransactionalTestCase to provide an slf4j logger object

--- thekey-cas-server-common/src/main/java/org/ccci/gcx/idm/common/spring2/test/AbstractTransactionalTestCase.java
+++ thekey-cas-server-common/src/main/java/org/ccci/gcx/idm/common/spring2/test/AbstractTransactionalTestCase.java
@@ -1,6 +1,8 @@
 package org.ccci.gcx.idm.common.spring2.test;
 
 import org.ccci.gcx.idm.common.spring2.ApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.AbstractTransactionalSpringContextTests;
 
 /**
@@ -10,8 +12,9 @@ import org.springframework.test.AbstractTransactionalSpringContextTests;
  *
  * @author Greg Crider  Oct 19, 2008  7:19:42 PM
  */
-public class AbstractTransactionalTestCase extends AbstractTransactionalSpringContextTests
-{
+public abstract class AbstractTransactionalTestCase extends
+	AbstractTransactionalSpringContextTests {
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
 
     /**
      * Override the default behavior to return the same context files as {@link ApplicationContext}.
