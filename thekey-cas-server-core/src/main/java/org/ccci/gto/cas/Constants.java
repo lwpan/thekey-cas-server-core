4ee67790c32e7d2491d6fa58d696b9e272ba2c31
Daniel Frett <daniel.frett@ccci.org>
2011-05-27 19:08:14 -0400

    added several LDAP attribute name constants

--- thekey-cas-server-core/src/main/java/org/ccci/gto/cas/Constants.java
+++ thekey-cas-server-core/src/main/java/org/ccci/gto/cas/Constants.java
@@ -9,4 +9,20 @@ public final class Constants {
 
     // Error Codes
     public static final String ERROR_STALEPASSWORD = "error.account.forcechangepassword";
+
+    // LDAP Attributes
+    public static final String LDAP_ATTR_EMAIL = "cn";
+    public static final String LDAP_ATTR_GUID = "extensionAttribute1";
+    public static final String LDAP_ATTR_FIRSTNAME = "givenName";
+    public static final String LDAP_ATTR_LASTNAME = "sn";
+    public static final String LDAP_ATTR_LOGINTIME = "loginTime";
+    public static final String LDAP_ATTR_USERID = "uid";
+    public static final String LDAP_ATTR_DOMAINSVISITED = "extensionAttribute2";
+    public static final String LDAP_ATTR_ADDITIONALGUIDS = "extensionAttribute3";
+    public static final String LDAP_ATTR_ADDITIONALDOMAINSVISITED = "extensionAttribute4";
+    public static final String LDAP_ATTR_GROUPS = "groupMembership";
+    public static final String LDAP_ATTR_ALLOWPASSWORDCHANGE = "passwordAllowChange";
+    public static final String LDAP_ATTR_LOGINDISABLED = "loginDisabled";
+    public static final String LDAP_ATTR_LOCKED = "lockedByIntruder";
+    public static final String LDAP_ATTR_STALEPASSWORD = "extensionAttribute5";
 }
