4077b3dc82c5ea6b963d9b7c97ef29a98c8f25e4
Daniel Frett <daniel.frett@ccci.org>
2011-06-01 14:07:14 -0400

    [#IDM-53] Update the Signup Controller to utilize the new createUser method

--- thekey-cas-server-webapp/src/main/java/org/ccci/gcx/idm/web/SignupWizardController.java
+++ thekey-cas-server-webapp/src/main/java/org/ccci/gcx/idm/web/SignupWizardController.java
@@ -1,7 +1,5 @@
 package org.ccci.gcx.idm.web;
 
-import java.util.Date;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,7 +12,6 @@ import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractWizardFormController;
-import org.springframework.web.util.WebUtils;
 
 /**
  * Provides signup wizard use case of sso web application
@@ -41,7 +38,6 @@ public class SignupWizardController extends AbstractWizardFormController {
 	
 	//define our command object.
 	public SignupWizardController() {
-
 		setCommandClass(SimpleLoginUser.class);
 		setCommandName("user");
 		//pages in our wizard: reference according to int id (0,1,2,3)
@@ -50,75 +46,43 @@ public class SignupWizardController extends AbstractWizardFormController {
 		"serviceSignupNameView",
 		"serviceSignupSuccessView" });
 		}
-		
 
-	
-	//setup our command object
-	protected Object formBackingObject(HttpServletRequest request) throws Exception{
+    /**
+     * Once the user clicks "finish" the wizard calls processFinish. Creates
+     * user using wizard gathered form data and returns to login page.
+     */
+    protected ModelAndView processFinish(final HttpServletRequest req,
+	    final HttpServletResponse res, final Object command,
+	    final BindException errors) throws Exception {
+	final SimpleLoginUser form = (SimpleLoginUser) command;
 
-		// * From what I can tell from the docs
-		 //*  formBackingObject() should only be called ONCE and the FBO session should be happening automatically... but...
-
-		SimpleLoginUser user = (SimpleLoginUser) WebUtils.getSessionAttribute(request, "FBO_user");
-		
-		if(user == null)
-		{
-			user = new SimpleLoginUser();
-			WebUtils.setSessionAttribute(request, "FBO_user", user);
-		}
-		hits++;
-		return user;
-
-	}
-
-	/**
-	 * Once the user clicks "finish" the wizard calls processFinish. Creates user using 
-	 * wizard gathered form data and returns to login page.
-	 */
-	protected ModelAndView processFinish(HttpServletRequest req,
-			HttpServletResponse res, Object command, BindException errors)
-			throws Exception {
-
-			SimpleLoginUser form = (SimpleLoginUser) command;
-		
-	        Date currentDate = new Date() ;
-
-	// create a new GcxUser
-	        GcxUser user = new GcxUser() ;
+	// generate a new GcxUser object
+	final GcxUser user = new GcxUser();
 	user.setGUID(RandomGUID.generateGuid(true));
-	        
-	        user.setCreateDate( currentDate ) ;
-	        user.setEmail( form.getUsername() ) ;
-	        //user.setPassword( "changeme" ) ; JIRA: IDM-11 - gcxuserservice now provides a random password.
-	        user.setFirstName( form.getFirstName() ) ;
-	        user.setLastName( form.getLastName() ) ;
-	        user.setForcePasswordChange( true ) ;
-	        user.setLocked( false ) ;
-	        user.setLoginDisabled( false ) ;
-	        user.setLoginTime( null ) ;
-	        user.setPasswordAllowChange( true ) ;
-	        user.setDomainsVisitedString("");
-	        user.setDomainsVisitedAdditionalString("");
-	        user.setGUIDAdditionalString("");
-	        
-	        /*= INFO =*/ if ( log.isInfoEnabled() ) log.info( "***** User: " + user ) ;
-	        /*= INFO =*/ if ( log.isInfoEnabled() ) log.info( "***** Preparing to create through service" ) ;
-	        
-	        gcxuserservice.createTransitionalUser( user, "SignupWizardController");
-		
+	user.setEmail(form.getUsername());
+	user.setFirstName(form.getFirstName());
+	user.setLastName(form.getLastName());
+	user.setPasswordAllowChange(true);
+	user.setForcePasswordChange(true);
+	user.setLoginDisabled(false);
+	user.setVerified(false);
+	// user.setPassword("randompassword");
 
-		req.getSession().removeAttribute("FBO_user"); //take out our fbo object
-		WebUtils.setSessionAttribute(req, Constants.SESSIONATTRIBUTE_LOGIN, form.getUsername()); //set this guy for the logincontroller
-
-		signups++;
-	return new ModelAndView("serviceSignupSuccessView");
+	// create the new user in the GcxUserService
+	if (logger.isInfoEnabled()) {
+	    logger.info("***** User: " + user);
+	    logger.info("***** Preparing to create through service");
 	}
-	
+	gcxuserservice.createUser(user, "SignupWizardController");
+
+	// return the signup success view
+	return new ModelAndView("serviceSignupSuccessView");
+    }
+
 	protected ModelAndView processCancel(HttpServletRequest req,
 			HttpServletResponse res, Object command, BindException errors)
 			throws Exception {
 		if(log.isDebugEnabled()) log.debug("cancel is selected!");
-		req.getSession().removeAttribute("FBO_user");
 	res.sendRedirect("/login");
 		return null;
 	}
@@ -147,7 +111,6 @@ public class SignupWizardController extends AbstractWizardFormController {
 	protected ModelAndView handleInvalidSubmit(HttpServletRequest request,
             HttpServletResponse response) throws Exception
 	{
-			request.getSession().removeAttribute("FBO_user");
 	response.sendRedirect("/login");
 			return null;
 	}
