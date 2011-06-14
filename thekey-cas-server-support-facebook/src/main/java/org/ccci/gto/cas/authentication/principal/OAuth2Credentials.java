2e4bb4f3ed0f622961e7cb7a8baf78cb950ba7e9
Daniel Frett <daniel.frett@ccci.org>
2011-06-14 18:08:01 -0400

    updated the OAuth2Credentials to implement TheKeyCredentials for granular administrative locks

--- thekey-cas-server-support-facebook/src/main/java/org/ccci/gto/cas/authentication/principal/OAuth2Credentials.java
+++ thekey-cas-server-support-facebook/src/main/java/org/ccci/gto/cas/authentication/principal/OAuth2Credentials.java
@@ -1,10 +1,14 @@
 package org.ccci.gto.cas.authentication.principal;
 
+import java.util.BitSet;
+
 import org.jasig.cas.authentication.principal.Credentials;
 
-public class OAuth2Credentials implements Credentials {
+public class OAuth2Credentials implements Credentials, TheKeyCredentials {
     /** Unique ID for serialization. */
     private static final long serialVersionUID = -4603224077053631621L;
+
+    private final BitSet locks = new BitSet();
 
     /** The Opaque authorization code */
     private final String code;
@@ -33,5 +37,17 @@ public class OAuth2Credentials implements Credentials {
      */
     public String getState() {
 	return this.state;
+    }
+
+    public void setObserveLock(final Lock lock, final boolean value) {
+	synchronized (locks) {
+	    locks.set(lock.index, !value);
+	}
+    }
+
+    public boolean observeLock(final Lock lock) {
+	synchronized (locks) {
+	    return !locks.get(lock.index);
+	}
     }
 }
