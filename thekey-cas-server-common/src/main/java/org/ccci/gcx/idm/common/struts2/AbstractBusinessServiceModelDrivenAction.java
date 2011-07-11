8cad8ce6cbb89a1816fd48faa20a5aae32e90574
Daniel Frett <daniel.frett@ccci.org>
2011-07-11 13:43:13 -0400

    deprecated setModelObject in favor of using setModel method

--- thekey-cas-server-common/src/main/java/org/ccci/gcx/idm/common/struts2/AbstractBusinessServiceModelDrivenAction.java
+++ thekey-cas-server-common/src/main/java/org/ccci/gcx/idm/common/struts2/AbstractBusinessServiceModelDrivenAction.java
@@ -5,43 +5,48 @@ import org.ccci.gcx.idm.common.model.ModelObject;
 import com.opensymphony.xwork2.ModelDriven;
 
 /**
- * <b>AbstractBusinessServiceModelDrivenAction</b> contains common functionality for Struts2
- * action classes using the {@link ModelDriven} interface. This interface allows a domain
- * model (of type {@link ModelObject} to be specified for use with the {@link Action} and 
- * corresponding paramters and properties.
- *
- * @author Greg Crider  Feb 4, 2008  2:23:12 PM
+ * <b>AbstractBusinessServiceModelDrivenAction</b> contains common functionality
+ * for Struts2 action classes using the {@link ModelDriven} interface. This
+ * interface allows a domain model (of type {@link ModelObject} to be specified
+ * for use with the {@link Action} and corresponding paramters and properties.
+ * 
+ * @author Greg Crider Feb 4, 2008 2:23:12 PM
  */
 public abstract class AbstractBusinessServiceModelDrivenAction<T extends ModelObject>
 	extends AbstractBusinessServicePreparableAction implements
 	ModelDriven<T> {
-    private static final long serialVersionUID = -909630855578788723L ;
+    private static final long serialVersionUID = -909630855578788723L;
 
     /** Default domain model entity */
-    private T m_ModelObject = null;
+    private T model;
 
     /**
      * Used by interceptor for access the domain model. Don't override this
      * method unless you know what you are doing.
      * 
-     * @return {@link ModelObject} returned by concrete implementation. 
+     * @return {@link ModelObject} returned by concrete implementation.
      * 
      * @see com.opensymphony.xwork2.ModelDriven#getModel()
      * @see AbstractBusinessServiceModelDrivenAction#getModelObject()
      */
     public T getModel() {
-	return this.m_ModelObject;
+	return this.model;
     }
 
     /**
-     * Set the {@link ModelObject} instance to be used as the domain model for the
-     * {@link ModelDriven} interface.
+     * Set the {@link ModelObject} instance to be used as the domain model for
+     * the {@link ModelDriven} interface.
      * 
-     * @param a_ModelObject {@link ModelObject} to be used with the {@link Action}.
+     * @param model
+     *            {@link ModelObject} to be used with the {@link Action}.
      */
-    public void setModelObject(T a_ModelObject) {
+    public void setModel(T model) {
 	log.trace("***** Setting ModelObject");
+	this.model = model;
+    }
 
-	this.m_ModelObject = a_ModelObject;
+    @Deprecated
+    public void setModelObject(T model) {
+	this.setModel(model);
     }
 }
