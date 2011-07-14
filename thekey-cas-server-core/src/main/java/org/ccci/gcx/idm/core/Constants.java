b5690e4ebba7c5523e7123146c54edb9d0642dcf
Daniel Frett <daniel.frett@ccci.org>
2011-07-14 18:49:01 -0400

    removed several unused constants

--- thekey-cas-server-core/src/main/java/org/ccci/gcx/idm/core/Constants.java
+++ thekey-cas-server-core/src/main/java/org/ccci/gcx/idm/core/Constants.java
@@ -7,7 +7,6 @@ package org.ccci.gcx.idm.core;
  */
 public interface Constants
 {
-
     // BEAN OBJECTS
     
     public static final String BEAN_AUDIT_DAO = "persist.auditDao" ;
@@ -19,8 +18,6 @@ public interface Constants
     public static final String BEAN_GCXUSER_DAO = "ldap.gcxUserDao" ;
     
     public static final String BEAN_GCXUSER_SERVICE = "service.gcxUserService" ;
-    
-    public static final String BEAN_AUTHENTICATION_SERVICE = "service.authenticationService";
 
     public static final String BEAN_RANDOM_PASSWORD = "util.randomPassword" ;
     
@@ -58,55 +55,11 @@ public interface Constants
     public static final String INTERNAL_CREATEDBY = "INTERNAL" ;
     
     //Authentication
-	public static final String LOGIN_URL = "/login";
-	public static final String LOGOUT_URL = "/logout";
-	
-	public static final String TICKET_BEGIN = "ticket=";
-	public static final String URL_BEGIN = "service=";
-	public static final String LT_REGEX = ".*name=\"lt\".value=\"(.*?)\"";
-	public static final String DEFAULTSERVICEPROTOCOL ="http://";
-	public static final String DEFAULTCASSERVICEPROTOCOL = "https://";
-	
-	public static final String SERVICE_VALIDATE_URL = "/serviceValidate";
-	public static final String PROXY_VALIDATE_URL = "/proxyValidate";
-	public static final String PROXY_TICKET_URL = "/proxy";
-	public static final String SYMBOL_AMP = "&";
-	public static final String PGTURL_BEGIN = "pgtUrl=";
-
-	public static final String CAS_SERVICE = "service";
-	public static final String CAS_TICKET  = "ticket";
-	public static final String CAS_PGT     = "pgtUrl";
-	public static final String CAS_EVENTID = "_eventId";
-	public static final String CAS_USERNAME = "username";
-	public static final String CAS_PASSWORD = "password";
-	public static final String CAS_LOGINTICKET = "lt";
-	public static final String CAS_GATEWAY    = "gateway";
-	public static final String CAS_LOCATIONHEADER = "location";
-	public static final String CAS_PROXY_TARGETSERVICE = "targetService=";
-	public static final String CAS_PROXY_TARGETSERVICE_PARAM = "targetService";
-	public static final String CAS_PROXY_PGT_PARAM = "pgt";
-	public static final String CAS_PROXY_PGT = "pgt=";
-	
-	public static final String CAS_TGC	= "CASTGC";
-	public static final String CAS_COOKIEPATH = "/internal";
-	public static final String SERVICE_PROCESSTGC = "processTGCRequest";
-	
 	public static final int    DEFAULTPROXY = 8080;
-	public static final String CAS_GATEWAYPARM = "&gateway=true";
 	
 	public static final String ERROR_NOCREDENTIALORPRINCIPAL = "error.nocredentialorprincipal";
 	public static final String ERROR_SERVICEEMPTY = "error.servicevalidate.serviceempty";
 	public static final String ERROR_TICKETEMPTY = "error.servicevalidate.ticketempty";
 	public static final String ERROR_VALIDATIONFAILED = "error.servicevalidate.failed";
 	public static final String ERROR_NOCOOKIEAFTERAUTH = "error.login.nocookieafterauth";
-
-	//public static final String CAS_COOKIEDOMAIN = ".mygcx.org";
-	public static final String CAS_DEFAULTCOOKIEDOMAIN = "localhost";
-
-	public static final String CAS_LOGOUTCALLBACK = "logoutCallback";
-
-	public static final int DEFAULTCOOKIEEXPIRY = 14400;
-
-	
-
 }
