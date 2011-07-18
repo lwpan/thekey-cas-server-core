28e56c6d3622612213cff251b88b9189848ebe12
Daniel Frett <daniel.frett@ccci.org>
2011-07-18 14:18:57 -0400

    updated auditable services to store the audit service directly in the object and not through a business service group

--- thekey-cas-server-core/src/main/java/org/ccci/gcx/idm/core/service/impl/AbstractAuditableService.java
+++ thekey-cas-server-core/src/main/java/org/ccci/gcx/idm/core/service/impl/AbstractAuditableService.java
@@ -1,26 +1,37 @@
 package org.ccci.gcx.idm.core.service.impl;
 
+import javax.validation.constraints.NotNull;
+
+import org.ccci.gcx.idm.common.service.BusinessService;
 import org.ccci.gcx.idm.common.service.impl.AbstractDataAccessService;
-import org.ccci.gcx.idm.core.Constants;
 import org.ccci.gcx.idm.core.service.AuditService;
 
 /**
- * <b>AbstractAuditableService</b> contains common functionality for all {@link BusinessService}
- * implementations that require the ability to be audited.
- *
- * @author Greg Crider  Oct 21, 2008  1:11:36 PM
+ * <b>AbstractAuditableService</b> contains common functionality for all
+ * {@link BusinessService} implementations that require the ability to be
+ * audited.
+ * 
+ * @author Daniel Frett
  */
-public abstract class AbstractAuditableService extends AbstractDataAccessService
-{
+public abstract class AbstractAuditableService extends
+	AbstractDataAccessService {
+    @NotNull
+    private AuditService auditService;
 
-    
     /**
-     * Convenience method to return the {@link AuditService}.
+     * retrieve the {@link AuditService}.
      * 
      * @return {@link AuditService}.
      */
-    protected AuditService getAuditService()
-    {
-        return (AuditService)this.getService( Constants.BEAN_AUDIT_SERVICE ) ;
+    protected AuditService getAuditService() {
+	return this.auditService;
+    }
+
+    /**
+     * @param auditService
+     *            the auditService to use
+     */
+    public void setAuditService(final AuditService auditService) {
+	this.auditService = auditService;
     }
 }
