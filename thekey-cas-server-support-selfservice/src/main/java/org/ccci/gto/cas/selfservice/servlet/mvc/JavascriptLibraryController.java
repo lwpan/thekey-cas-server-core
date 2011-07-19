7ca17df217a5811dfa32aafa77712aff2d239669
Daniel Frett <daniel.frett@ccci.org>
2011-07-19 14:45:56 -0400

    update the JavascriptLibraryController to not cache the javascript locally, it should be cached by the PasswordValidator where it can be updated when the validator is updated

--- thekey-cas-server-support-selfservice/src/main/java/org/ccci/gto/cas/selfservice/servlet/mvc/JavascriptLibraryController.java
+++ thekey-cas-server-support-selfservice/src/main/java/org/ccci/gto/cas/selfservice/servlet/mvc/JavascriptLibraryController.java
@@ -4,64 +4,38 @@ import static org.ccci.gto.cas.selfservice.Constants.SESSION_ATTR_PASSWORDJAVASC
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.NotNull;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.ccci.gto.cas.selfservice.validator.PasswordValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 import org.springframework.web.util.WebUtils;
 
 /**
- * Provides client javascript. Since it only changes on a configuration change, 
- * we can store the compiled string in a singleton value.  we'll also stuff it into each client session
- * for easy access from the jsps.
- * 
- * NOTE: Currently this is serving up only one javascript type, so if more come along, they should be
- *       served up from this controller (and refactoring of the pattern will be in order)
- * @author ken
- *
+ * Provides the password validation client javascript.
  */
-public class JavascriptLibraryController implements Controller 
-{
-	protected static final Log log = LogFactory.getLog(JavascriptLibraryController.class);
+public class JavascriptLibraryController implements Controller {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
 
-	private PasswordValidator pwv;
-	private String clientjavascript; //store our singleton value for quick access. 
-	
-	//DI
-	public void setPasswordValidator(PasswordValidator a_pwv)
-	{
-		pwv = a_pwv;
-	}
-	
-	/**
-	 * handles a request for a javascript library that isn't a static page.
-	 */
+    @NotNull
+    private PasswordValidator validator;
+
+    public void setPasswordValidator(final PasswordValidator validator) {
+	this.validator = validator;
+    }
+
+    /**
+     * handles a request for a javascript library that isn't a static page.
+     */
     public ModelAndView handleRequest(final HttpServletRequest request,
 	    final HttpServletResponse response) throws Exception {
-	if (StringUtils.isEmpty(clientjavascript)) {
-	    clientjavascript = pwv.getValidationJavascript();
-	}
-		
-		//stuff the clientjavascript into the 
-	if (StringUtils.isEmpty((String) WebUtils.getSessionAttribute(request,
-		SESSION_ATTR_PASSWORDJAVASCRIPT))) {
-			if(log.isDebugEnabled()) log.debug("setting the client javascript");
-	    WebUtils.setSessionAttribute(request,
-		    SESSION_ATTR_PASSWORDJAVASCRIPT, clientjavascript);
-			//log.debug(pwv.getClientJavascript());
-		}    
-	     
+	// stuff the clientjavascript into the session
+	log.debug("setting the client javascript");
+	WebUtils.setSessionAttribute(request, SESSION_ATTR_PASSWORDJAVASCRIPT,
+		validator.getValidationJavascript());
+
 	return new ModelAndView("javascriptPasswordValidation");
-	}
-	
-	/*
-	 * clears the cache.
-	 */
-	public void clearCache()
-	{
-		clientjavascript = null;
-	}
+    }
 }
