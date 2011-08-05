2a86f3ef93ebd3e58ebd6d07c9f95e7e14e831a8
Daniel Frett <daniel.frett@ccci.org>
2011-08-05 19:05:39 -0400

    [#IDM-77] updated the FacebookAuthenticationHandler to utilize the signedRequest to determine if this was a valid authentication or not

--- thekey-cas-server-support-facebook/src/main/java/org/ccci/gto/cas/authentication/handler/FacebookAuthenticationHandler.java
+++ thekey-cas-server-support-facebook/src/main/java/org/ccci/gto/cas/authentication/handler/FacebookAuthenticationHandler.java
@@ -1,10 +1,26 @@
 package org.ccci.gto.cas.authentication.handler;
 
+import javax.validation.constraints.NotNull;
+
 import org.ccci.gto.cas.authentication.principal.FacebookCredentials;
 import org.ccci.gto.cas.authentication.principal.OAuth2Credentials;
+import org.ccci.gto.cas.facebook.util.FacebookUtils;
 import org.jasig.cas.authentication.handler.AuthenticationException;
 
+import com.restfb.json.JsonObject;
+
 public class FacebookAuthenticationHandler extends OAuth2AuthenticationHandler {
+    @NotNull
+    private String secret;
+
+    /**
+     * @param secret
+     *            the secret to set
+     */
+    public void setSecret(final String secret) {
+	this.secret = secret;
+    }
+
     public FacebookAuthenticationHandler() {
 	super(FacebookCredentials.class, false);
     }
@@ -19,9 +35,22 @@ public class FacebookAuthenticationHandler extends OAuth2AuthenticationHandler {
     protected boolean authenticateOAuth2Internal(
 	    final OAuth2Credentials rawCreds) throws AuthenticationException {
 	final FacebookCredentials credentials = (FacebookCredentials) rawCreds;
-	// TODO: implement logic to retrieve access_token using the OAuth2
-	// authorization code
 
-	return credentials.getAccessToken() != null;
+	// extract the json data from the signed request, which is stored as the
+	// code for FacebookCredentials
+	final String signedRequest = credentials.getSignedRequest();
+	final JsonObject data = FacebookUtils.getSignedData(signedRequest);
+
+	// validate the signed request
+	if (!FacebookUtils.validateSignedRequest(signedRequest, this.secret,
+		data.optString("algorithm"))) {
+	    return false;
+	}
+
+	if (data.optString("user_id", null) == null) {
+	    return false;
+	}
+
+	return true;
     }
 }
