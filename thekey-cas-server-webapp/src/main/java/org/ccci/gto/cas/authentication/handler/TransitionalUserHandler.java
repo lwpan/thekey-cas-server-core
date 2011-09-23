57e9b76f9ad845a2b83a9eaf5598dab716974d87
Daniel Frett <daniel.frett@ccci.org>
2011-09-23 15:15:22 -0400

    moved the Constants import to be a static import

--- thekey-cas-server-webapp/src/main/java/org/ccci/gto/cas/authentication/handler/TransitionalUserHandler.java
+++ thekey-cas-server-webapp/src/main/java/org/ccci/gto/cas/authentication/handler/TransitionalUserHandler.java
@@ -1,10 +1,11 @@
 package org.ccci.gto.cas.authentication.handler;
 
+import static org.ccci.gcx.idm.web.Constants.SOURCEIDENTIFIER_ACTIVATION;
+
 import javax.validation.constraints.NotNull;
 
 import org.ccci.gcx.idm.core.model.impl.GcxUser;
 import org.ccci.gcx.idm.core.service.GcxUserService;
-import org.ccci.gcx.idm.web.Constants;
 import org.jasig.cas.authentication.handler.PasswordEncoder;
 import org.jasig.cas.authentication.handler.support.AbstractUsernamePasswordAuthenticationHandler;
 import org.jasig.cas.authentication.principal.UsernamePasswordCredentials;
@@ -41,7 +42,7 @@ public class TransitionalUserHandler extends
 		// matched, so activate this transitional user.
 		log.info("Activating transitional user: " + userName);
 		gcxUserService.activateTransitionalUser(user,
-			Constants.SOURCEIDENTIFIER_ACTIVATION, userName);
+			SOURCEIDENTIFIER_ACTIVATION, userName);
 
 		// return success for authentication
 		return true;
