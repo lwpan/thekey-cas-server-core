8f627168934fb6d9a86484376353c1fad0ed1e50
Daniel Frett <daniel.frett@ccci.org>
2011-09-23 17:38:14 -0400

    [#IDM-88] create a UserDetailsService class that maps Key users to Acegi users

---
+++ thekey-cas-server-support-admin/src/main/java/org/ccci/gto/cas/admin/security/userdetails/TheKeyUserDetailsService.java
@@ -0,0 +1,71 @@
+package org.ccci.gto.cas.admin.security.userdetails;
+
+import java.util.TreeSet;
+
+import javax.validation.constraints.NotNull;
+
+import org.ccci.gcx.idm.core.model.impl.GcxUser;
+import org.ccci.gcx.idm.core.service.GcxUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class TheKeyUserDetailsService implements UserDetailsService {
+    /** Instance of logging for subclasses. */
+    protected Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @NotNull
+    private GcxUserService userService;
+
+    @NotNull
+    private String adminGroupDn;
+
+    /**
+     * @param adminGroupDn
+     *            the adminGroupDn to set
+     */
+    public void setAdminGroupDn(final String adminGroupDn) {
+	this.adminGroupDn = adminGroupDn;
+    }
+
+    /**
+     * @param userService
+     *            the userService to set
+     */
+    public void setUserService(final GcxUserService userService) {
+	this.userService = userService;
+    }
+
+    /**
+     * @return the userService
+     */
+    protected GcxUserService getUserService() {
+	return this.userService;
+    }
+
+    public UserDetails loadUserByUsername(final String username)
+	    throws UsernameNotFoundException, DataAccessException {
+	// look up the specified user
+	final GcxUser keyUser = this.getUserService().findUserByEmail(username);
+	if (keyUser == null) {
+	    throw new UsernameNotFoundException("User not found: " + username,
+		    username);
+	}
+
+	// setup the user's authority
+	final TreeSet<GrantedAuthority> authority = new TreeSet<GrantedAuthority>();
+	if (keyUser.getGroupMembership().contains(this.adminGroupDn)) {
+	    authority.add(new GrantedAuthorityImpl("ROLE_ADMIN"));
+	}
+
+	// create and return a user object
+	return new User(keyUser.getEmail(), "", true, true, true, true,
+		authority);
+    }
+}
