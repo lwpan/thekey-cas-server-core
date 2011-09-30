5455267376f857e7872b819f3f817f25f3c07417
Daniel Frett <daniel.frett@ccci.org>
2011-09-30 14:05:20 -0400

    [#IDM-91] add abstract Dao classes for JPA support

---
+++ thekey-cas-server-common/src/main/java/org/ccci/gto/persist/jpa/AbstractCrudDao.java
@@ -0,0 +1,24 @@
+package org.ccci.gto.persist.jpa;
+
+import java.util.Collection;
+
+import org.ccci.gto.persist.CrudDao;
+import org.springframework.util.Assert;
+
+public abstract class AbstractCrudDao<T> extends AbstractQueryDao<T> implements
+	CrudDao<T> {
+    public abstract void save(final T object);
+
+    public abstract void saveOrUpdate(final T object);
+
+    public abstract void update(final T object);
+
+    public abstract void delete(final T object);
+
+    public void saveAll(final Collection<? extends T> objects) {
+	Assert.notEmpty(objects);
+	for (final T entry : objects) {
+	    this.save(entry);
+	}
+    }
+}
