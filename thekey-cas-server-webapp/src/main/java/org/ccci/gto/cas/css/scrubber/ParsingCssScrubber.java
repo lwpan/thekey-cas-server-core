0ec4d921013bddf31bd7e9ff7bfe5e884c5a798e
Daniel Frett <daniel.frett@ccci.org>
2011-10-07 15:35:11 -0400

    [#IDM-101] implement a ParsingCssScrubber object
    right now this depends on a SNAPSHOT build of cssparser

---
+++ thekey-cas-server-webapp/src/main/java/org/ccci/gto/cas/css/scrubber/ParsingCssScrubber.java
@@ -0,0 +1,202 @@
+package org.ccci.gto.cas.css.scrubber;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.ccci.gcx.idm.web.css.AbstractCssScrubber;
+import org.w3c.css.sac.InputSource;
+import org.w3c.dom.css.CSSRule;
+import org.w3c.dom.css.CSSRuleList;
+import org.w3c.dom.css.CSSStyleDeclaration;
+import org.w3c.dom.css.CSSStyleRule;
+import org.w3c.dom.css.CSSStyleSheet;
+
+import com.steadystate.css.dom.CSSStyleDeclarationImpl;
+import com.steadystate.css.dom.Property;
+import com.steadystate.css.parser.CSSOMParser;
+
+public class ParsingCssScrubber extends AbstractCssScrubber {
+    private final HashSet<String> blockedProperties = new HashSet<String>();
+    private final ArrayList<Pattern> blockedPropertyValues = new ArrayList<Pattern>();
+    private final HashSet<Short> blockedTypes = new HashSet<Short>();
+
+    public void addBlockedProperty(final String name) {
+	if (name != null) {
+	    this.blockedProperties.add(name);
+	}
+    }
+
+    public void setBlockedProperties(final Collection<? extends String> names) {
+	this.blockedProperties.clear();
+	if (names != null) {
+	    this.blockedProperties.addAll(names);
+	}
+    }
+
+    public void addBlockedPropertyValue(final String pattern) {
+	if (pattern != null) {
+	    this.blockedPropertyValues.add(Pattern.compile(pattern));
+	}
+    }
+
+    public void setBlockedPropertyValues(
+	    final Collection<? extends String> patterns) {
+	this.blockedPropertyValues.clear();
+	if (patterns != null) {
+	    for (final String pattern : patterns) {
+		if (pattern != null) {
+		    this.blockedPropertyValues.add(Pattern.compile(pattern));
+		}
+	    }
+	}
+    }
+
+    public void addBlockedType(final Short type) {
+	this.blockedTypes.add(type);
+    }
+
+    public void setBlockedTypes(final Collection<? extends Short> types) {
+	this.blockedTypes.clear();
+	if (types != null) {
+	    this.blockedTypes.addAll(types);
+	}
+    }
+
+    protected CSSStyleSheet parse(final InputSource source) {
+	try {
+	    final CSSOMParser parser = new CSSOMParser();
+	    return parser.parseStyleSheet(source, null, null);
+	} catch (final IOException e) {
+	    log.error("error parsing CSS", e);
+	}
+
+	return null;
+    }
+
+    @Override
+    public String scrub(final URI uri) {
+	final CSSStyleSheet css = this.scrub(new InputSource(uri.toString()));
+	return css.toString();
+    }
+
+    protected CSSStyleSheet scrub(final InputSource source) {
+	final CSSStyleSheet css = this.parse(source);
+
+	// iterate over all the CSS rules
+	final CSSRuleList rules = css.getCssRules();
+	for (int i = 0; i < rules.getLength(); i++) {
+	    final CSSRule rule = rules.item(i);
+
+	    // is this a blocked rule type?
+	    if (blockedTypes.contains(rule.getType())) {
+		css.deleteRule(i);
+		i--;
+		continue;
+	    }
+
+	    switch (rule.getType()) {
+	    case CSSRule.STYLE_RULE:
+		if (this.blockedProperties.size() > 0
+			|| this.blockedPropertyValues.size() > 0) {
+		    this.scrubStyleRule((CSSStyleRule) rule);
+		}
+		break;
+	    }
+	}
+
+	return css;
+    }
+
+    private void scrubStyleRule(final CSSStyleRule rule) {
+	final CSSStyleDeclaration styles = rule.getStyle();
+
+	// the set of properties to remove from this rule
+	final HashSet<String> propertiesToRemove = new HashSet<String>();
+
+	// remove any blocked properties
+	propertiesToRemove.addAll(blockedProperties);
+
+	/* only process the properties if there are blocked values */
+	if (this.blockedPropertyValues.size() > 0) {
+	    /*
+	     * for {@link CSSStyleDeclarationImpl} we can directly manipulate
+	     * the Property objects to get around the standard API limitations
+	     */
+	    if (styles instanceof CSSStyleDeclarationImpl) {
+		final List<Property> properties = ((CSSStyleDeclarationImpl) styles)
+			.getProperties();
+		for (int j = 0; j < properties.size(); j++) {
+		    final Property property = properties.get(j);
+		    final String value = property.getValue().toString();
+		    for (final Pattern pattern : this.blockedPropertyValues) {
+			if (pattern.matcher(value).find()) {
+			    properties.remove(j);
+			    j--;
+			    break;
+			}
+		    }
+		}
+	    } else {
+		final HashSet<String> seen = new HashSet<String>();
+		for (int j = 0; j < styles.getLength(); j++) {
+		    final String name = styles.item(j);
+		    final String value = styles.getPropertyValue(name);
+
+		    /*
+		     * remove any duplicate properties because the standard API
+		     * doesn't provide a way to manipulate properties with the
+		     * same name that are not the first property
+		     */
+		    if (seen.contains(name.toLowerCase())) {
+			propertiesToRemove.add(name);
+		    }
+
+		    /*
+		     * check to see if the value matches a blocked pattern
+		     */
+		    for (final Pattern pattern : this.blockedPropertyValues) {
+			if (pattern.matcher(value).find()) {
+			    propertiesToRemove.add(name);
+			    break;
+			}
+		    }
+
+		    /*
+		     * add this property to the list of seen properties
+		     */
+		    seen.add(name.toLowerCase());
+		}
+
+		/*
+		 * remove any properties that have values that match the blocked
+		 * patterns
+		 */
+		for (int j = 0; j < styles.getLength(); j++) {
+		    final String name = styles.item(j);
+		    log.debug(name + ": " + styles.getPropertyValue(name));
+		}
+	    }
+	}
+
+	/* remove all properties that need to be removed */
+	for (final String property : propertiesToRemove) {
+	    /*
+	     * removeProperty only removes the first property with the specified
+	     * name, loop until all properties using the specified name are
+	     * removed
+	     */
+	    while (true) {
+		final int properties = styles.getLength();
+		styles.removeProperty(property);
+		if (properties == styles.getLength()) {
+		    break;
+		}
+	    }
+	}
+    }
+}
---
+++ thekey-cas-server-webapp/src/test/java/org/ccci/gto/cas/css/scrubber/ParsingCssScrubberTest.java
@@ -0,0 +1,148 @@
+package org.ccci.gto.cas.css.scrubber;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+
+import junit.framework.TestCase;
+
+import org.w3c.css.sac.InputSource;
+import org.w3c.dom.css.CSSRule;
+import org.w3c.dom.css.CSSRuleList;
+import org.w3c.dom.css.CSSStyleRule;
+import org.w3c.dom.css.CSSStyleSheet;
+
+public class ParsingCssScrubberTest extends TestCase {
+    private ParsingCssScrubber getCssScrubber() {
+	final ParsingCssScrubber scrubber = new ParsingCssScrubber();
+	scrubber.setBlockedProperties(null);
+	scrubber.setBlockedTypes(null);
+	scrubber.setBlockedPropertyValues(null);
+	return scrubber;
+    }
+
+    private InputSource getFileInputSource(final String fileName) {
+	final InputStream is = getClass().getClassLoader().getResourceAsStream(
+		fileName);
+	assertNotNull(is);
+	return new InputSource(new InputStreamReader(is));
+    }
+
+    private InputSource getStringInputSource(final String css) {
+	return new InputSource(new StringReader(css));
+    }
+
+    public void testParser() {
+	final ParsingCssScrubber scrubber = this.getCssScrubber();
+
+	{
+	    final CSSStyleSheet css = scrubber
+		    .parse(getStringInputSource(".foo { font-weight: normal !/* comment */important; }"));
+	    assertEquals(1, css.getCssRules().getLength());
+	    final CSSRule rule = css.getCssRules().item(0);
+	    assertEquals(CSSRule.STYLE_RULE, rule.getType());
+	    assertEquals(1, ((CSSStyleRule) rule).getStyle().getLength());
+	}
+    }
+
+    public void testBlockRules() {
+	final ParsingCssScrubber scrubber = this.getCssScrubber();
+	final String RULES = "@import url('a'); .foo { behavior: test2; -mm-behavior: test2; font-weight: normal !/* comment */important; behavior: test; font-weight: expression(a); }";
+	final int RULES_TOTAL = 2;
+	final int RULES_IMPORT = 1;
+	final int ATTRS_TOTAL = 5;
+	final int ATTRS_BEHAVIOR = 2;
+	final int ATTRS_EXPRESSION = 1;
+
+	/* nothing blocked */
+	{
+	    final CSSStyleSheet css = scrubber
+		    .scrub(getStringInputSource(RULES));
+	    final CSSRuleList rules = css.getCssRules();
+	    final CSSRule rule = rules.item(rules.getLength() - 1);
+	    assertEquals(RULES_TOTAL, rules.getLength());
+	    assertEquals(CSSRule.STYLE_RULE, rule.getType());
+	    assertEquals(ATTRS_TOTAL, ((CSSStyleRule) rule).getStyle()
+		    .getLength());
+	}
+
+	/* block import rules */
+	{
+	    scrubber.addBlockedType(CSSRule.IMPORT_RULE);
+	    final CSSStyleSheet css = scrubber
+		    .scrub(getStringInputSource(RULES));
+	    final CSSRuleList rules = css.getCssRules();
+	    final CSSRule rule = rules.item(rules.getLength() - 1);
+	    assertEquals(RULES_TOTAL - RULES_IMPORT, rules.getLength());
+	    assertEquals(CSSRule.STYLE_RULE, rule.getType());
+	    assertEquals(ATTRS_TOTAL, ((CSSStyleRule) rule).getStyle()
+		    .getLength());
+	    scrubber.setBlockedTypes(null);
+	}
+
+	/* block behavior properties */
+	{
+	    scrubber.addBlockedProperty("behavior");
+	    final CSSStyleSheet css = scrubber
+		    .scrub(getStringInputSource(RULES));
+	    final CSSRuleList rules = css.getCssRules();
+	    final CSSRule rule = rules.item(rules.getLength() - 1);
+	    assertEquals(RULES_TOTAL, rules.getLength());
+	    assertEquals(CSSRule.STYLE_RULE, rule.getType());
+	    assertEquals(ATTRS_TOTAL - ATTRS_BEHAVIOR, ((CSSStyleRule) rule)
+		    .getStyle().getLength());
+	    scrubber.setBlockedProperties(null);
+	}
+
+	/* block import rules and behavior properties */
+	{
+	    scrubber.addBlockedType(CSSRule.IMPORT_RULE);
+	    scrubber.addBlockedProperty("behavior");
+	    final CSSStyleSheet css = scrubber
+		    .scrub(getStringInputSource(RULES));
+	    final CSSRuleList rules = css.getCssRules();
+	    final CSSRule rule = rules.item(rules.getLength() - 1);
+	    assertEquals(RULES_TOTAL - RULES_IMPORT, rules.getLength());
+	    assertEquals(CSSRule.STYLE_RULE, rule.getType());
+	    assertEquals(ATTRS_TOTAL - ATTRS_BEHAVIOR, ((CSSStyleRule) rule)
+		    .getStyle().getLength());
+	    scrubber.setBlockedProperties(null);
+	    scrubber.setBlockedTypes(null);
+	}
+
+	/* block properties containing expressions */
+	{
+	    scrubber.addBlockedPropertyValue("expression");
+	    final CSSStyleSheet css = scrubber
+		    .scrub(getStringInputSource(RULES));
+	    final CSSRuleList rules = css.getCssRules();
+	    final CSSRule rule = rules.item(rules.getLength() - 1);
+	    assertEquals(RULES_TOTAL, rules.getLength());
+	    assertEquals(CSSRule.STYLE_RULE, rule.getType());
+	    assertEquals(ATTRS_TOTAL - ATTRS_EXPRESSION, ((CSSStyleRule) rule)
+		    .getStyle().getLength());
+	    scrubber.setBlockedPropertyValues(null);
+	}
+
+	/*
+	 * block import rules, behavior properties, and properties containing
+	 * expressions
+	 */
+	{
+	    scrubber.addBlockedType(CSSRule.IMPORT_RULE);
+	    scrubber.addBlockedProperty("behavior");
+	    scrubber.addBlockedPropertyValue("expression");
+	    final CSSStyleSheet css = scrubber
+		    .scrub(getStringInputSource(RULES));
+	    final CSSRuleList rules = css.getCssRules();
+	    final CSSRule rule = rules.item(rules.getLength() - 1);
+	    assertEquals(RULES_TOTAL - RULES_IMPORT, rules.getLength());
+	    assertEquals(CSSRule.STYLE_RULE, rule.getType());
+	    assertEquals(ATTRS_TOTAL - ATTRS_BEHAVIOR - ATTRS_EXPRESSION,
+		    ((CSSStyleRule) rule).getStyle().getLength());
+	    scrubber.setBlockedPropertyValues(null);
+	    scrubber.setBlockedProperties(null);
+	    scrubber.setBlockedTypes(null);
+	}
+    }
+}
