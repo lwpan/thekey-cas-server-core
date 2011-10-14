3b2cfb8d6098c7a77ae1ee6f17d8fce3e9f2eec8
Daniel Frett <daniel.frett@ccci.org>
2011-10-14 15:53:23 -0400

    replace commons logging with slf4j

--- thekey-cas-server-support-css/src/main/java/org/ccci/gto/cas/css/servlet/CssServiceController.java
+++ thekey-cas-server-support-css/src/main/java/org/ccci/gto/cas/css/servlet/CssServiceController.java
@@ -8,10 +8,10 @@ import java.net.URI;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.ccci.gto.cas.css.scrubber.CachingCssScrubber;
 import org.ccci.gto.cas.css.scrubber.CssScrubber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
@@ -29,8 +29,8 @@ import org.springframework.web.servlet.mvc.Controller;
  *
  */
 public class CssServiceController implements Controller{
-
-	protected static final Log log = LogFactory.getLog(CssServiceController.class);
+    /** Instance of logging for subclasses. */
+    protected final Logger log = LoggerFactory.getLogger(getClass());
 	
 	//DI
 	private CssScrubber scrubber;
