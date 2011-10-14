bf9be3451bf5240c16b7b33dae9808cdb36d61b3
Daniel Frett <daniel.frett@ccci.org>
2011-10-14 16:07:29 -0400

    cleaned up the CssServiceController

--- thekey-cas-server-support-css/src/main/java/org/ccci/gto/cas/css/servlet/CssServiceController.java
+++ thekey-cas-server-support-css/src/main/java/org/ccci/gto/cas/css/servlet/CssServiceController.java
@@ -3,10 +3,13 @@ package org.ccci.gto.cas.css.servlet;
 import static org.ccci.gto.cas.css.Constants.PARAMETER_CSS_URI;
 import static org.ccci.gto.cas.css.Constants.PARAMETER_RELOAD_CSS;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.NotNull;
 
 import org.ccci.gto.cas.css.scrubber.CachingCssScrubber;
 import org.ccci.gto.cas.css.scrubber.CssScrubber;
@@ -16,73 +19,65 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-/**
- * Receives a request for css and uses DI configured scrubber. Returns whatever
- * the scrubber gives us back. 
- * NOTE: we don't actually scrub anything in our current implementation...  We just
- * return NOTHING if we find any of the prohibited strings.
- *
- * feature: reload parameter.  reload=true or reload=yes should trigger a cache reload for this css url.
- *
- * Always returns a null ModelAndView and manually prints to the response output.
- * @author ken
- *
- */
-public class CssServiceController implements Controller{
+public class CssServiceController implements Controller {
     /** Instance of logging for subclasses. */
     protected final Logger log = LoggerFactory.getLogger(getClass());
-	
-	//DI
-	private CssScrubber scrubber;
-	
-	public ModelAndView handleRequest(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
 
-		response.setContentType("text/css");
-		
-		if(log.isDebugEnabled())  log.debug("determining css location");
-	final String uri = request.getParameter(PARAMETER_CSS_URI);
+    @NotNull
+    private CssScrubber scrubber;
+
+    /**
+     * @param scrubber
+     *            the css scrubber to use
+     */
+    public void setScrubber(final CssScrubber scrubber) {
+	this.scrubber = scrubber;
+    }
+
+    public ModelAndView handleRequest(final HttpServletRequest request,
+	    final HttpServletResponse response) {
+	// we always return CSS, even if it's an empty response
+	response.setContentType("text/css");
+
+	// parse the uri to see if it's valid
+	final URI uri;
+	try {
+	    uri = new URI(request.getParameter(PARAMETER_CSS_URI));
+	} catch (final URISyntaxException e) {
+	    log.debug("invalid CSS uri specified", e);
+	    return null;
+	}
+
+	// fetch the requested CSS
 	final boolean reload = StringUtils.hasText(request
 		.getParameter(PARAMETER_RELOAD_CSS));
+	final String css = scrubCssContent(uri, reload);
 
-	if (!StringUtils.hasText(uri)) {
-			if(log.isDebugEnabled())  log.debug("didn't find a css parameter");
-			response.getOutputStream().print("");
-			return null;
-		}
-		
-	final String css = scrubCssContent(new URI(uri), reload);
-	response.getOutputStream().print(css);
-		
-		return null;
+	// Output the scrubbed CSS
+	if (css != null) {
+	    try {
+		response.getWriter().print(css);
+	    } catch (final IOException e) {
+		log.debug("Error outputing CSS", e);
+	    }
 	}
+
+	// return null indicating no further processing is required
+	return null;
+    }
 
     /**
      * scrub css content
      * 
-     * @param cssUri
+     * @param uri
      * @param reload
      * @return
      */
-    private String scrubCssContent(final URI cssUri, boolean reload) {
+    private String scrubCssContent(final URI uri, final boolean reload) {
 	if (reload && scrubber instanceof CachingCssScrubber) {
 	    log.debug("reload requested");
-	    ((CachingCssScrubber) scrubber).removeFromCache(cssUri);
+	    ((CachingCssScrubber) scrubber).removeFromCache(uri);
 	}
-	return scrubber.scrub(cssUri);
+	return scrubber.scrub(uri);
     }
-
-	/**
-	 * @return the scrubber
-	 */
-	public CssScrubber getScrubber() {
-		return scrubber;
-	}
-
-	/**
-	 * @param scrubber the scrubber to set
-	 */
-	public void setScrubber(CssScrubber scrubber) {
-		this.scrubber = scrubber;
-	}
 }
