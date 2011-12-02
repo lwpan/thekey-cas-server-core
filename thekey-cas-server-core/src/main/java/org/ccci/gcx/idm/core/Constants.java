30f9aaa2e5289dcab0d674e21f0cda3865409d5f
Daniel Frett <daniel.frett@ccci.org>
2011-12-02 12:15:51 -0500

    removed a bunch of unused constants

--- thekey-cas-server-core/src/main/java/org/ccci/gcx/idm/core/Constants.java
+++ thekey-cas-server-core/src/main/java/org/ccci/gcx/idm/core/Constants.java
@@ -7,35 +7,6 @@ package org.ccci.gcx.idm.core;
  */
 public interface Constants
 {
-    // BEAN OBJECTS
-    
-    public static final String BEAN_AUDIT_DAO = "persist.auditDao" ;
-    
-    public static final String BEAN_AUDIT_SERVICE = "service.auditService" ;
-    
-    public static final String BEAN_TRANS_GCXUSER_DAO = "persist.gcxUserDao" ;
-    
-    public static final String BEAN_GCXUSER_DAO = "ldap.gcxUserDao" ;
-    
-    public static final String BEAN_GCXUSER_SERVICE = "service.gcxUserService" ;
-
-    public static final String BEAN_MAIL_SENDER = "mail.mailSender" ;
-    
-    public static final String BEAN_MAIL_SERVICE = "service.mailService" ;
-    
-    public static final String BEAN_MESSAGE_SOURCE = "util.messageSource" ;
-
-    // HIBERNATE QUERY NAMES
-    
-    public static final String QUERY_AUDIT_FINDALLBYUSERID = "org.ccci.gcx.idm.core.model.impl.Audit.findAllByUserid" ;
-    
-    public static final String QUERY_GCXUSER_FINDBYGUID = "org.ccci.gcx.idm.core.model.impl.GcxUser.findByGuid" ;
-    
-    public static final String QUERY_GCXUSER_FINDBYEMAIL = "org.ccci.gcx.idm.core.model.impl.GcxUser.findByEmail" ;
-    
-    // LDAP
-    public static final String LDAP_KEY_PASSWORD = "userPassword" ;
-    
     // SEARCH
     
     public static final int SEARCH_NO_LIMIT = 0 ;
@@ -49,13 +20,4 @@ public interface Constants
     public static final String INTERNAL_SOURCE = "INTERNAL" ;
     
     public static final String INTERNAL_CREATEDBY = "INTERNAL" ;
-    
-    //Authentication
-	public static final int    DEFAULTPROXY = 8080;
-	
-	public static final String ERROR_NOCREDENTIALORPRINCIPAL = "error.nocredentialorprincipal";
-	public static final String ERROR_SERVICEEMPTY = "error.servicevalidate.serviceempty";
-	public static final String ERROR_TICKETEMPTY = "error.servicevalidate.ticketempty";
-	public static final String ERROR_VALIDATIONFAILED = "error.servicevalidate.failed";
-	public static final String ERROR_NOCOOKIEAFTERAUTH = "error.login.nocookieafterauth";
 }
