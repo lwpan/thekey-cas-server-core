f6c1aca745b8f329e6ffe9fb96d11fdc668bb156
Daniel Frett <daniel.frett@ccci.org>
2011-12-12 16:43:21 -0500

    [#IDM-143] fadeOut the help dialog when the user clicks elsewhere

--- thekey-cas-server-webapp/src/main/webapp/themes/thekey/helpPopup.js
+++ thekey-cas-server-webapp/src/main/webapp/themes/thekey/helpPopup.js
@@ -1,17 +1,25 @@
 jQuery(document).ready(function($) {
 	var popup = $('#ssoHelp_popup-wrap');
 
-	$("#ssoHelp_link").click(function(){
+	$("#ssoHelp_link").click(function(event){
 		popup.fadeToggle(300);
+		event.stopPropagation()
 	});
 
-	$("#ssoHelp_link-active").click(function(){
+	$("#ssoHelp_link-active").click(function(event){
 		popup.fadeOut(300);
+		event.stopPropagation()
 	});
 
-	$(document.documentElement).keyup(function(e){
-		if(e.keyCode === 27){
+	$(document.documentElement).keyup(function(event){
+		if(event.keyCode === 27){
 			popup.fadeOut(300);
 		}
+	});
+
+	$('body').click(function(event) {
+		if (!$(event.target).closest('#ssoHelp_popup').length) {
+			popup.fadeOut(300);
+		};
 	});
 });
