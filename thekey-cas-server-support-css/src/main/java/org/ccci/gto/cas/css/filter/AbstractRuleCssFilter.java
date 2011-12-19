4289384b215a85d3be068c254d7f0000157dc65a
Daniel Frett <daniel.frett@ccci.org>
2011-12-19 14:44:01 -0500

    [#IDM-170] created a RuleType CssFilter that can filter based on specific css rule types

---
+++ thekey-cas-server-support-css/src/main/java/org/ccci/gto/cas/css/filter/AbstractRuleCssFilter.java
@@ -0,0 +1,40 @@
+package org.ccci.gto.cas.css.filter;
+
+import java.net.URI;
+
+import org.w3c.dom.css.CSSRule;
+import org.w3c.dom.css.CSSRuleList;
+import org.w3c.dom.css.CSSStyleSheet;
+
+public abstract class AbstractRuleCssFilter extends AbstractCssFilter {
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.ccci.gto.cas.css.filter.AbstractCssFilter#filterInternal(org.w3c.
+     * dom.css.CSSStyleSheet, java.net.URI)
+     */
+    @Override
+    protected final CSSStyleSheet filterInternal(final CSSStyleSheet css,
+	    final URI uri) {
+	// iterate over all the CSS rules
+	final CSSRuleList rules = css.getCssRules();
+	for (int i = 0; i < rules.getLength(); i++) {
+	    final CSSRule rule = rules.item(i);
+
+	    // is this a blocked rule type?
+	    if (this.isBlocked(rule)) {
+		css.deleteRule(i);
+		i--;
+		continue;
+	    }
+	}
+
+	return css;
+    }
+
+    protected boolean isBlocked(final CSSRule rule) {
+	// default to the rule being allowed
+	return false;
+    }
+}
---
+++ thekey-cas-server-support-css/src/main/java/org/ccci/gto/cas/css/filter/RuleTypeCssFilter.java
@@ -0,0 +1,42 @@
+package org.ccci.gto.cas.css.filter;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.w3c.dom.css.CSSRule;
+
+public final class RuleTypeCssFilter extends AbstractRuleCssFilter implements
+	ReversibleFilter {
+    private Type type = null;
+    private final HashSet<Short> types = new HashSet<Short>();
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.ccci.gto.cas.css.filter.ReversibleFilter#setFilterType(org.ccci.gto
+     * .cas.css.filter.ReversibleFilter.Type)
+     */
+    @Override
+    public void setFilterType(final Type type) {
+	this.type = type;
+    }
+
+    public void addType(final Short type) {
+	this.types.add(type);
+    }
+
+    public void setTypes(final Collection<? extends Short> types) {
+	this.types.clear();
+	if (types != null) {
+	    this.types.addAll(types);
+	}
+    }
+
+    @Override
+    protected boolean isBlocked(final CSSRule rule) {
+	return type == null
+		|| (type == Type.BLACKLIST && types.contains(rule.getType()))
+		|| (type == Type.WHITELIST && !types.contains(rule.getType()));
+    }
+}
