de3c0aea2cd61d871c44aded695d4abd2ecd787f
Daniel Frett <daniel.frett@ccci.org>
2011-12-19 14:37:54 -0500

    [#IDM-170] created a ReversibleFilter interface
    ReversibleFilters can act as whitelists or blacklists

---
+++ thekey-cas-server-support-css/src/main/java/org/ccci/gto/cas/css/filter/ReversibleFilter.java
@@ -0,0 +1,15 @@
+package org.ccci.gto.cas.css.filter;
+
+public interface ReversibleFilter {
+    public enum Type {
+	WHITELIST(0), BLACKLIST(1);
+
+	public final int index;
+
+	Type(final int index) {
+	    this.index = index;
+	}
+    }
+
+    public void setFilterType(final Type type);
+}
