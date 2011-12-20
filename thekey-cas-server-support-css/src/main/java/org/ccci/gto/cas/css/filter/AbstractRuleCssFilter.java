44b9140a16c1c01a68450a099eb039c9c8403f8b
Daniel Frett <daniel.frett@ccci.org>
2011-12-20 11:26:06 -0500

    [#IDM-170] media rules contain other rules, we should filter them as well to be thorough

--- thekey-cas-server-support-css/src/main/java/org/ccci/gto/cas/css/filter/AbstractRuleCssFilter.java
+++ thekey-cas-server-support-css/src/main/java/org/ccci/gto/cas/css/filter/AbstractRuleCssFilter.java
@@ -1,5 +1,6 @@
 package org.ccci.gto.cas.css.filter;
 
+import org.w3c.dom.css.CSSMediaRule;
 import org.w3c.dom.css.CSSRule;
 import org.w3c.dom.css.CSSRuleList;
 import org.w3c.dom.css.CSSStyleSheet;
@@ -14,6 +15,34 @@ public abstract class AbstractRuleCssFilter extends AbstractCssFilter {
      */
     @Override
     protected final CSSStyleSheet filterInternal(final CSSStyleSheet css) {
+	this.filterStyleSheet(css);
+	return css;
+    }
+
+    private void filterMediaRule(final CSSMediaRule mediaRule) {
+	// iterate over all the CSS rules
+	final CSSRuleList rules = mediaRule.getCssRules();
+	for (int i = 0; i < rules.getLength(); i++) {
+	    final CSSRule rule = rules.item(i);
+
+	    // is this a blocked rule type?
+	    if (this.isBlocked(rule)) {
+		mediaRule.deleteRule(i);
+		i--;
+		continue;
+	    }
+
+	    // process nested rules in @media rules
+	    if (rule instanceof CSSMediaRule) {
+		this.filterMediaRule((CSSMediaRule) rule);
+	    }
+
+	    // apply any filters to the current CSSRule
+	    this.filterRule(rule);
+	}
+    }
+
+    private void filterStyleSheet(final CSSStyleSheet css) {
 	// iterate over all the CSS rules
 	final CSSRuleList rules = css.getCssRules();
 	for (int i = 0; i < rules.getLength(); i++) {
@@ -25,9 +54,19 @@ public abstract class AbstractRuleCssFilter extends AbstractCssFilter {
 		i--;
 		continue;
 	    }
-	}
 
-	return css;
+	    // process nested rules in @media rules
+	    if (rule instanceof CSSMediaRule) {
+		this.filterMediaRule((CSSMediaRule) rule);
+	    }
+
+	    // apply any filters to the current CSSRule
+	    this.filterRule(rule);
+	}
+    }
+
+    protected void filterRule(final CSSRule rule) {
+	// do nothing by default
     }
 
     protected boolean isBlocked(final CSSRule rule) {
