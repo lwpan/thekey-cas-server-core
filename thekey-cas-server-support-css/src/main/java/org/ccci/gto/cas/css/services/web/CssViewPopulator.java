35cb720387916efddf80a54aa34f53dcd4b3a3d2
Daniel Frett <daniel.frett@ccci.org>
2011-12-21 16:21:51 -0500

    [#IDM-172] make sure to set templateUri to the value stored in the session when no template is specified

--- thekey-cas-server-support-css/src/main/java/org/ccci/gto/cas/css/services/web/CssViewPopulator.java
+++ thekey-cas-server-support-css/src/main/java/org/ccci/gto/cas/css/services/web/CssViewPopulator.java
@@ -5,8 +5,8 @@ import static org.ccci.gto.cas.css.Constants.SESSION_TEMPLATEURL;
 import static org.ccci.gto.cas.css.Constants.URL_DEFAULT_TEMPLATE;
 import static org.ccci.gto.cas.css.Constants.VIEW_ATTR_TEMPLATEURL;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -24,62 +24,66 @@ public final class CssViewPopulator extends AbstractViewPopulator {
 	final Service service = context.getService();
 
 	// set the template to use for this request
-	URL templateUrl = null;
+	URI templateUri = null;
 
 	// check for a specified template first
-	if (templateUrl == null) {
+	if (templateUri == null) {
 	    final String template = request.getParameter(PARAMETER_TEMPLATEURL);
 	    if (StringUtils.isNotBlank(template)) {
 		try {
-		    templateUrl = new URL(template);
-		} catch (final MalformedURLException e) {
+		    templateUri = new URI(template);
+		} catch (final URISyntaxException e) {
 		    log.debug("Malformed template parameter", e);
-		    templateUrl = null;
+		    templateUri = null;
 		}
 	    }
 	}
 
 	// check for the template URL in the RegisteredService
-	if (templateUrl == null) {
+	if (templateUri == null) {
 	    final RegisteredService rService = context.getRegisteredService();
 	    if (rService instanceof TheKeyRegisteredService) {
 		final String template = ((TheKeyRegisteredService) rService)
 			.getTemplateCssUrl();
 		if (StringUtils.isNotBlank(template)) {
 		    try {
-			templateUrl = new URL(template);
-		    } catch (final MalformedURLException e) {
+			templateUri = new URI(template);
+		    } catch (final URISyntaxException e) {
 			log.debug(
 				"Invalid templateCssUrl in RegisteredService",
 				e);
-			templateUrl = null;
+			templateUri = null;
 		    }
 		}
 	    }
 	}
 
 	// set a default template url based on the specified service
-	if (templateUrl == null && service != null) {
+	if (templateUri == null && service != null) {
 	    try {
-		final URL serviceUrl = new URL(service.getId());
-		templateUrl = new URL(serviceUrl, URL_DEFAULT_TEMPLATE);
-	    } catch (final MalformedURLException e) {
+		final URI serviceUri = new URI(service.getId());
+		templateUri = serviceUri.resolve(URL_DEFAULT_TEMPLATE);
+	    } catch (final URISyntaxException e) {
 		log.debug("Error generating default template url from service",
 			e);
-		templateUrl = null;
+		templateUri = null;
 	    }
 	}
 
-	// load the template url from the session
-	if (templateUrl == null) {
-	    request.getSession().getAttribute(SESSION_TEMPLATEURL);
+	// load the template uri from the session
+	if (templateUri == null) {
+	    final Object attr = request.getSession().getAttribute(
+		    SESSION_TEMPLATEURL);
+	    if (attr instanceof URI) {
+		templateUri = (URI) attr;
+	    }
 	}
 	// otherwise store the found templateUrl in the session
 	else {
-	    request.getSession().setAttribute(SESSION_TEMPLATEURL, templateUrl);
+	    request.getSession().setAttribute(SESSION_TEMPLATEURL, templateUri);
 	}
 
 	// set the template url in the view context
-	context.setAttribute(VIEW_ATTR_TEMPLATEURL, templateUrl);
+	context.setAttribute(VIEW_ATTR_TEMPLATEURL, templateUri);
     }
 }
