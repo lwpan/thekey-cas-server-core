51a631e70188b03921cf59c22fcc092bef7f5f18
Daniel Frett <daniel.frett@ccci.org>
2011-12-28 13:36:10 -0500

    cleaned out more unused functionality in IdmException

--- thekey-cas-server-common/src/main/java/org/ccci/gcx/idm/common/IdmException.java
+++ thekey-cas-server-common/src/main/java/org/ccci/gcx/idm/common/IdmException.java
@@ -1,9 +1,5 @@
 package org.ccci.gcx.idm.common ;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 /**
  * <b>IdmException</b> is used to report one or more error messages within a single
  * unchecked {@link Exception}. In addition, certain meta data can be added in
@@ -14,82 +10,6 @@ import java.util.Map;
 public class IdmException extends RuntimeException {
     private static final long serialVersionUID = 1900633246071420616L ;
 
-    /** Hold list of individual exceptions */
-    private List<String> m_ErrorList = null ;
-
-    /** Meta data relevant to the underlying error */
-    private Map<String,Object> m_MetaData = new HashMap<String,Object>() ;
-
-    /** Original message of exception; does not include the enumerated error list */
-    private String m_OriginalMessage = null ;
-
-    /**
-     * Get the list of errors.
-     *
-     * @return {@link List} of error messages.
-     */
-    public List<String> getErrors()
-    {
-        return this.m_ErrorList ;
-    }
-
-
-    /**
-     * Are there any enumerated errors?
-     *
-     * @return <tt>True</tt> if there is a list of errors available.
-     */
-    public boolean hasErrors()
-    {
-        return ( ( this.m_ErrorList != null ) && ( this.m_ErrorList.size() > 0 ) ) ;
-    }
-
-
-    /**
-     * Return the meta data.
-     *
-     * @return {@link Map} of meta data.
-     */
-    public Map<String,Object> getMetaData()
-    {
-        return this.m_MetaData ;
-    }
-
-
-    /**
-     * Add a new meta data value.
-     *
-     * @param a_Key Key to meta data.
-     * @param a_Value Value of meta data.
-     */
-    public void addMetaData( String a_Key, Object a_Value )
-    {
-        this.m_MetaData.put( a_Key, a_Value ) ;
-    }
-
-
-    /**
-     * Is there any meta data?
-     *
-     * @return <tt>True</tt> if there is meta data present.
-     */
-    public boolean hasMetaData()
-    {
-        return ( this.m_MetaData.size() > 0 ) ;
-    }
-
-
-    /**
-     * Return the original message passed into the exception.
-     *
-     * @return Original message.
-     */
-    public String getOriginalMessage()
-    {
-        return this.m_OriginalMessage ;
-    }
-
-
     /**
      * Exception with message.
      *
@@ -98,10 +18,7 @@ public class IdmException extends RuntimeException {
     public IdmException( String a_Message )
     {
         super( a_Message ) ;
-
-        this.m_OriginalMessage = a_Message ;
     }
-
 
     /**
      * Exception with external cause.
@@ -112,7 +29,5 @@ public class IdmException extends RuntimeException {
     public IdmException( String a_Message, Throwable a_Cause )
     {
         super( a_Message, a_Cause ) ;
-
-        this.m_OriginalMessage = a_Message ;
     }
 }
