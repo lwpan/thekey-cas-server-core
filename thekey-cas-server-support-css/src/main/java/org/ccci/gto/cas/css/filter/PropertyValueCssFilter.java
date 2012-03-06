713a0f104d28e69e3b7c21d69eac3041b2a58434
Daniel Frett <daniel.frett@ccci.org>
2012-03-06 18:02:39 -0500

    added a basic test for the PropertyValueCssFilter

--- thekey-cas-server-support-css/src/main/java/org/ccci/gto/cas/css/filter/PropertyValueCssFilter.java
+++ thekey-cas-server-support-css/src/main/java/org/ccci/gto/cas/css/filter/PropertyValueCssFilter.java
@@ -2,16 +2,12 @@ package org.ccci.gto.cas.css.filter;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
 import java.util.regex.Pattern;
 
 import org.w3c.dom.css.CSSStyleDeclaration;
+import org.w3c.dom.css.CSSValue;
 
-import com.steadystate.css.dom.CSSStyleDeclarationImpl;
-import com.steadystate.css.dom.Property;
-
-public final class PropertyValueCssFilter extends AbstractStyleCssFilter
+public final class PropertyValueCssFilter extends AbstractValueCssFilter
 	implements ReversibleFilter {
     private Type type = null;
     private final ArrayList<Pattern> values = new ArrayList<Pattern>();
@@ -46,63 +42,29 @@ public final class PropertyValueCssFilter extends AbstractStyleCssFilter
 	return false;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.ccci.gto.cas.css.filter.AbstractValueCssFilter#filterValue(org.w3c
+     * .dom.css.CSSStyleDeclaration, org.w3c.dom.css.CSSValue)
+     */
     @Override
-    protected void filterStyles(final CSSStyleDeclaration styles) {
-	if (this.type != null) {
-	    /*
-	     * for {@link CSSStyleDeclarationImpl} we can directly manipulate
-	     * the Property objects to get around the standard API limitations
-	     */
-	    if (styles instanceof CSSStyleDeclarationImpl) {
-		final List<Property> properties = ((CSSStyleDeclarationImpl) styles)
-			.getProperties();
-		for (int j = 0; j < properties.size(); j++) {
-		    final Property property = properties.get(j);
-		    final boolean matches = this.checkValue(property.getValue()
-			    .toString());
-		    if ((this.type == Type.BLACKLIST && matches)
-			    || (this.type == Type.WHITELIST && !matches)) {
-			properties.remove(j);
-			j--;
-		    }
-		}
-	    } else {
-		// the set of properties to remove from this rule
-		final HashSet<String> propertiesToRemove = new HashSet<String>();
-
-		final HashSet<String> seen = new HashSet<String>();
-		for (int j = 0; j < styles.getLength(); j++) {
-		    final String name = styles.item(j);
-
-		    /*
-		     * remove any duplicate properties because the standard API
-		     * doesn't provide a way to manipulate properties that share
-		     * the same name in a rule
-		     */
-		    if (seen.contains(name.toLowerCase())) {
-			propertiesToRemove.add(name);
-			continue;
-		    }
-
-		    /*
-		     * add this property to the list of seen properties
-		     */
-		    seen.add(name.toLowerCase());
-
-		    /*
-		     * check to see if the value matches
-		     */
-		    final boolean matches = this.checkValue(styles
-			    .getPropertyValue(name));
-		    if ((this.type == Type.BLACKLIST && matches)
-			    || (this.type == Type.WHITELIST && matches)) {
-			propertiesToRemove.add(name);
-		    }
-		}
-
-		// remove any properties that have invalid values
-		this.removeProperties(styles, propertiesToRemove);
-	    }
+    protected boolean filterValue(final CSSStyleDeclaration styles,
+	    final String name, final CSSValue value) {
+	// short-circuit if a filter type wasn't set
+	if (this.type == null) {
+	    return true;
 	}
+
+	// check to see if this value matches the specified patterns
+	final boolean matches = this.checkValue(value.getCssText());
+	if ((this.type == Type.BLACKLIST && matches)
+		|| (this.type == Type.WHITELIST && !matches)) {
+	    return false;
+	}
+
+	// return the default value
+	return super.filterValue(styles, name, value);
     }
 }
