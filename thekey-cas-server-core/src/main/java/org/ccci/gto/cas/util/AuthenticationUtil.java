1f930b4bfc52dbd1ebeba4fe6d9882dca81034e2
Daniel Frett <daniel.frett@ccci.org>
2012-08-02 14:06:47 -0400

    [#IDM-216] added a checkLocks method that will check locks on some credentials

--- thekey-cas-server-core/src/main/java/org/ccci/gto/cas/util/AuthenticationUtil.java
+++ thekey-cas-server-core/src/main/java/org/ccci/gto/cas/util/AuthenticationUtil.java
@@ -3,10 +3,21 @@ package org.ccci.gto.cas.util;
 import static org.ccci.gto.cas.Constants.AUTH_ATTR_KEYUSER;
 
 import org.ccci.gcx.idm.core.model.impl.GcxUser;
+import org.ccci.gto.cas.authentication.handler.DeactivatedAccountAuthenticationException;
+import org.ccci.gto.cas.authentication.handler.DisabledAccountAuthenticationException;
+import org.ccci.gto.cas.authentication.handler.LockedAccountAuthenticationException;
+import org.ccci.gto.cas.authentication.principal.TheKeyCredentials;
+import org.ccci.gto.cas.authentication.principal.TheKeyCredentials.Lock;
 import org.jasig.cas.authentication.Authentication;
 import org.jasig.cas.authentication.MutableAuthentication;
+import org.jasig.cas.authentication.handler.AuthenticationException;
+import org.jasig.cas.authentication.handler.UnknownUsernameAuthenticationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class AuthenticationUtil {
+    private static final Logger LOG = LoggerFactory.getLogger(AuthenticationUtil.class);
+
     /**
      * This method guarantees the specified Authentication object has mutable
      * attributes
@@ -44,5 +55,38 @@ public final class AuthenticationUtil {
      */
     public static final GcxUser getUser(final Authentication authentication) {
 	return (GcxUser) authentication.getAttributes().get(AUTH_ATTR_KEYUSER);
+    }
+
+    public static final boolean checkLocks(final TheKeyCredentials credentials) throws AuthenticationException {
+        final GcxUser user = credentials.getGcxUser();
+
+        // the user authenticated, but doesn't exist?
+        if (user == null && credentials.observeLock(Lock.NULLUSER)) {
+            LOG.info("User authenticated, but account doesn't exist?!?!?");
+            throw UnknownUsernameAuthenticationException.ERROR;
+        }
+
+        // check for any administrative locks on the user
+        if (user != null) {
+            // is the account deactivated
+            if (user.isDeactivated() && credentials.observeLock(Lock.DEACTIVATED)) {
+                LOG.debug("Account is deactivated: {}", user.getGUID());
+                throw DeactivatedAccountAuthenticationException.ERROR;
+            }
+
+            // is the account locked
+            if (user.isLocked() && credentials.observeLock(Lock.LOCKED)) {
+                LOG.debug("Account is locked: {}", user.getGUID());
+                throw LockedAccountAuthenticationException.ERROR;
+            }
+
+            // is the account disabled
+            if (user.isLoginDisabled() && credentials.observeLock(Lock.DISABLED)) {
+                LOG.info("Account is disabled: {}", user.getGUID());
+                throw DisabledAccountAuthenticationException.ERROR;
+            }
+        }
+
+        return true;
     }
 }
