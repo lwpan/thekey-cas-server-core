0456db973aa27f2ce7d3a8804941362576c83eec
Daniel Frett <daniel.frett@ccci.org>
2014-06-16 11:24:43 -0400

    removed an unused method from the AuditDao

--- thekey-cas-server-core/src/main/java/org/ccci/gto/cas/persist/AuditDao.java
+++ thekey-cas-server-core/src/main/java/org/ccci/gto/cas/persist/AuditDao.java
@@ -1,25 +1,12 @@
 package org.ccci.gto.cas.persist;
 
-import java.util.List;
-
 import org.ccci.gto.cas.model.Audit;
 import org.ccci.gto.persist.CrudDao;
 
 /**
  * <b>AuditDao</b> defines the {@link CrudDao} for persisting and retrieving
  * {@link Audit} entities.
- * 
+ *
  * @author Daniel Frett
  */
-public interface AuditDao extends CrudDao<Audit> {
-    /**
-     * Find all {@link Audit} objects for the specified userid.
-     * 
-     * @param a_Userid
-     *            Userid for lookup.
-     * 
-     * @return {@link List} of {@link Audit} objects for userid, or
-     *         <tt>null</tt> if none are found.
-     */
-    public List<Audit> findAllByUserid(String a_Userid);
-}
+public interface AuditDao extends CrudDao<Audit> {}
--- thekey-cas-server-core/src/main/java/org/ccci/gto/cas/persist/jpa/JpaAuditDao.java
+++ thekey-cas-server-core/src/main/java/org/ccci/gto/cas/persist/jpa/JpaAuditDao.java
@@ -1,24 +1,20 @@
 package org.ccci.gto.cas.persist.jpa;
 
-import java.io.Serializable;
-import java.util.List;
-
 import org.ccci.gto.cas.model.Audit;
 import org.ccci.gto.cas.persist.AuditDao;
 import org.ccci.gto.persist.jpa.AbstractCrudDao;
 
+import java.io.Serializable;
+
 public class JpaAuditDao extends AbstractCrudDao<Audit> implements AuditDao {
+    @Override
     public Audit get(Serializable key) {
 	throw new UnsupportedOperationException(
 		"This method is not currently implemented.");
     }
 
+    @Override
     public Audit load(Serializable key) {
-	throw new UnsupportedOperationException(
-		"This method is not currently implemented.");
-    }
-
-    public List<Audit> findAllByUserid(String a_Userid) {
 	throw new UnsupportedOperationException(
 		"This method is not currently implemented.");
     }
